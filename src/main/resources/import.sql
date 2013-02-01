d9fec5ec2e61a1602970ee293fbd9e5b164ce0c6
Ricardo Valeriano <ricardo.valeriano@gmail.com>
2013-02-01 12:37:38 -0200

    Slugged title  no import

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,9 +5,9 @@ insert into User (createdAt, email, password) values ('20120104', 'artur.adam@ca
 insert into User (createdAt, email, password) values ('20120104', 'ricardo.valeriano@caelum.com.br', '2145a8274d61bd4377c8f98c2d8ab4c5ca93243a61ffddd03885fe990a740d57');
 insert into User (createdAt, email, password) values ('20120104', 'felipe.torres@caelum.com.br', '2145a8274d61bd4377c8f98c2d8ab4c5ca93243a61ffddd03885fe990a740d57');
 
-insert into Question (createdAt, description, title, author_id) values ('20130101', 'Como faz para dançar gangnam style?', 'OPPA?', 1);
-insert into Question (createdAt, description, title, author_id) values ('20130101', 'Como faz para assistir anime?', 'Death Note é maneiro?', 2);
-insert into Question (createdAt, description, title, author_id) values ('20130101', 'Como faz para pegar o saleiro da mão de outra pessoa?', 'Saleiro voador pode?', 3);
-insert into Question (createdAt, description, title, author_id) values ('20130101', 'Como faz para diminuir o alcoolismo quando ele começou aos 11 anos?', 'Bêbado que nem uma porca pode?', 4);
-insert into Question (createdAt, description, title, author_id) values ('20130101', 'Como faz para ter um humor um pouco melhor pelas manhãs?', '7 hora da manhã já pode estar puto?', 5);
-insert into Question (createdAt, description, title, author_id) values ('20130101', 'Como faz para parar de usar android e linux?', 'Usar celular e computador que funciona, pode?', 6);
+insert into Question (createdAt, description, title, author_id, sluggedTitle) values ('20130101', 'Como faz para dançar gangnam style?', 'OPPA?', 1, 'omg-lol-1');
+insert into Question (createdAt, description, title, author_id, sluggedTitle) values ('20130101', 'Como faz para assistir anime?', 'Death Note é maneiro?', 2, 'omg-lol-2');
+insert into Question (createdAt, description, title, author_id, sluggedTitle) values ('20130101', 'Como faz para pegar o saleiro da mão de outra pessoa?', 'Saleiro voador pode?', 3, 'omg-lol-3');
+insert into Question (createdAt, description, title, author_id, sluggedTitle) values ('20130101', 'Como faz para diminuir o alcoolismo quando ele começou aos 11 anos?', 'Bêbado que nem uma porca pode?', 4, 'omg-lol-4');
+insert into Question (createdAt, description, title, author_id, sluggedTitle) values ('20130101', 'Como faz para ter um humor um pouco melhor pelas manhãs?', '7 hora da manhã já pode estar puto?', 5, 'omg-lol-5');
+insert into Question (createdAt, description, title, author_id, sluggedTitle) values ('20130101', 'Como faz para parar de usar android e linux?', 'Usar celular e computador que funciona, pode?', 6, 'omg-lol-6');
