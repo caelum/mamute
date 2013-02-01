868f59e04ab772c46889a0535f5e3572b9350104
leonardowolter <leocwolter@gmail.com>
2013-02-01 17:21:40 -0200

    fixando ajax de marcar answer

--- src/main/webapp/js/ajax.js
+++ src/main/webapp/js/ajax.js
@@ -1,19 +1,27 @@
 $(function(){
-	$(".mark-as-solution").on('click', function(event){
+	$(".mark-as-solution").on('click', markAsSolution);
+	
+	function markAsSolution(event){
 		event.preventDefault();
 		var id = $(this).closest(".answer").data("id");
 		var mark = $(this);
-		$.post($(this).attr("href")+id, function(data){
-			$(mark).remove();
-			$(".answer").each(function(){
-				if($(this).hasClass("solution")){
-					var newHref= mark.attr("href")+$(this).data("id");
-					var newMark = mark.clone().attr("href", newHref);
-					$(this).removeClass("solution")
-							.append(newMark);
-				}
-			})
+		$.post($(this).attr("href")+id,function(){updateMarks(mark)});
+	};
+	
+	function updateMarks(mark){
+		var solution = $(mark).closest(".answer");
+		solution.addClass("solution");
+		$(mark).remove();
+		$(".answer").not(solution).each(function(){
+			console.log($(this));
+			if($(this).hasClass("solution")){
+				var newMark = mark.clone();
+				newMark.on('click', markAsSolution);
+				$(this).removeClass("solution")
+						.append(newMark);
+				
+			}
 		});
-	});
+	}
 	
 });
