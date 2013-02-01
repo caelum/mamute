691d0412c57e766fa6e20dc730bb82116ebe3aab
leonardowolter <leocwolter@gmail.com>
2013-02-01 11:05:12 -0200

    completando answer

--- src/main/java/br/com/caelum/brutal/model/Answer.java
+++ src/main/java/br/com/caelum/brutal/model/Answer.java
@@ -3,6 +3,7 @@ package br.com.caelum.brutal.model;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
+import javax.persistence.ManyToOne;
 
 import org.hibernate.annotations.Type;
 import org.joda.time.DateTime;
@@ -16,6 +17,14 @@ public class Answer {
 	@Type(type = "org.joda.time.contrib.hibernate.PersistentDateTime")
 	private final DateTime createdAt = new DateTime();
 
+	@Type(type="text")
 	private String text;
+	
+	@ManyToOne
+	private User author;
+	
+	@ManyToOne
+	private Question question;
+	
 	
 }
