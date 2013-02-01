9a55b1a1552602a96f40917cca711c7eb2e94431
leonardowolter <leocwolter@gmail.com>
2013-02-01 12:21:16 -0200

    adicionando sluggedTitle no import.sql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -5,9 +5,9 @@ insert into User (createdAt, email, password) values ('20120104', 'artur.adam@ca
 insert into User (createdAt, email, password) values ('20120104', 'ricardo.valeriano@caelum.com.br', '2145a8274d61bd4377c8f98c2d8ab4c5ca93243a61ffddd03885fe990a740d57');
 insert into User (createdAt, email, password) values ('20120104', 'felipe.torres@caelum.com.br', '2145a8274d61bd4377c8f98c2d8ab4c5ca93243a61ffddd03885fe990a740d57');
 
-insert into Question (createdAt, description, title, author_id, views) values ('20130101', 'Como faz para dan�ar gangnam style?', 'OPPA?', 1,0);
-insert into Question (createdAt, description, title, author_id, views) values ('20130101', 'Como faz para assistir anime?', 'Death Note � maneiro?', 2,0);
-insert into Question (createdAt, description, title, author_id, views) values ('20130101', 'Como faz para pegar o saleiro da m�o de outra pessoa?', 'Saleiro voador pode?', 3,0);
-insert into Question (createdAt, description, title, author_id, views) values ('20130101', 'Como faz para diminuir o alcoolismo quando ele come�ou aos 11 anos?', 'B�bado que nem uma porca pode?', 4,0);
-insert into Question (createdAt, description, title, author_id, views) values ('20130101', 'Como faz para ter um humor um pouco melhor pelas manh�s?', '7 hora da manh� j� pode estar puto?', 5,0);
-insert into Question (createdAt, description, title, author_id, views) values ('20130101', 'Como faz para parar de usar android e linux?', 'Usar celular e computador que funciona, pode?', 6,0);
+insert into Question (createdAt, description, sluggedTitle, title, author_id, views) values ('20130101', 'Como faz para dan�ar gangnam style?', 'como-faz-para-danar-gangnam-style', 'OPPA?', 1, 0);
+insert into Question (createdAt, description, sluggedTitle, title, author_id, views) values ('20130101', 'Como faz para assistir anime?', 'como-faz-para-assistir-anime','Death Note � maneiro?', 2, 0);
+insert into Question (createdAt, description, sluggedTitle, title, author_id, views) values ('20130101', 'Como faz para pegar o saleiro da m�o de outra pessoa?', 'como-faz-para-pegar-o-saleiro-da-mao-de-outra-pessoa', 'Saleiro voador pode?', 3, 0);
+insert into Question (createdAt, description, sluggedTitle, title, author_id, views) values ('20130101', 'Como faz para diminuir o alcoolismo quando ele come�ou aos 11 anos?', 'como-faz-para-diminuir-o-alcolismo-quando-ele-comecou-aos-11-anos', 'B�bado que nem uma porca pode?', 4, 0);
+insert into Question (createdAt, description, sluggedTitle, title, author_id, views) values ('20130101', 'Como faz para ter um humor um pouco melhor pelas manh�s?', 'como-faz-para-ter-um-humor-um-pouco-melhor-pelas-manhas', '7 hora da manh� j� pode estar puto?', 5, 0);
+insert into Question (createdAt, description, sluggedTitle, title, author_id, views) values ('20130101', 'Como faz para parar de usar android e linux?', 'como-faz-para-parar-de-usar-android-e-linux', 'Usar celular e computador que funciona, pode?', 6, 0);
