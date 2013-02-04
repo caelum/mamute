e99123882327e07f0dd88308bce204f5a5f57019
leonardowolter <leocwolter@gmail.com>
2013-02-04 12:41:25 -0200

    removendo request que nao precisa mais

--- src/main/java/br/com/caelum/brutal/controllers/ListController.java
+++ src/main/java/br/com/caelum/brutal/controllers/ListController.java
@@ -4,8 +4,6 @@ import static br.com.caelum.vraptor.view.Results.page;
 
 import java.util.List;
 
-import javax.servlet.http.HttpServletRequest;
-
 import br.com.caelum.brutal.dao.QuestionDAO;
 import br.com.caelum.brutal.dao.TagDAO;
 import br.com.caelum.brutal.model.Question;
@@ -20,13 +18,11 @@ public class ListController {
 	private final QuestionDAO questions;
 	private final Result result;
 	private final TagDAO tags;
-	private final HttpServletRequest req;
 
-	public ListController(QuestionDAO questions, TagDAO tags, Result result, HttpServletRequest req) {
+	public ListController(QuestionDAO questions, TagDAO tags, Result result) {
 		this.questions = questions;
 		this.tags = tags;
 		this.result = result;
-		this.req = req;
 	}
 
 	@Get("/")
