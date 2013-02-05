a4f7d3a5588d4bc50462a1dfd9f6f5715fbb7d05
leonardowolter <leocwolter@gmail.com>
2013-02-05 12:10:43 -0200

    removendo algumas coisinhas que nao faziam mais sentido

--- src/main/java/br/com/caelum/brutal/controllers/Updater.java
+++ src/main/java/br/com/caelum/brutal/controllers/Updater.java
@@ -3,20 +3,13 @@ package br.com.caelum.brutal.controllers;
 import br.com.caelum.brutal.model.Question;
 import br.com.caelum.brutal.model.QuestionInformation;
 import br.com.caelum.brutal.model.UpdateStatus;
-import br.com.caelum.brutal.model.User;
 import br.com.caelum.vraptor.ioc.Component;
 
 @Component
 public class Updater {
 
-    private final User currentUser;
-
-    public Updater(User currentUser) {
-        this.currentUser = currentUser;
-    }
-
 	public UpdateStatus update(Question original, QuestionInformation question) {
-        UpdateStatus status = currentUser.canUpdate(original);
+        UpdateStatus status = question.getAuthor().canUpdate(original);
         if (status == UpdateStatus.REFUSED)
             return status;
         
