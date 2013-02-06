6e9c9f697d159bd69de23e7b8654c5bd4efe990d
leonardowolter <leocwolter@gmail.com>
2013-02-06 19:23:28 -0200

    mensagens de validacao em portugues

--- src/main/webapp/js/form-validation.js
+++ src/main/webapp/js/form-validation.js
@@ -1,4 +1,10 @@
 $(function(){
+	jQuery.extend(jQuery.validator.messages, {
+	    required: "Este campo é necessário.",
+	    minlength: jQuery.validator.format("Por favor, insira ao menos {0} caracteres."),
+	});
+	
+	
 	$(".validated-form").each(function(i,f){
 		$(f).validate();
 	});
