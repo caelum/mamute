37c00ec2900243189cf0e72d8cd6d92b716f363d
Felipe Torres <felipe.gtorres@gmail.com>
2013-02-08 17:46:39 -0200

    Mudando a cor do submenu de acordo com a pagina atual.

---
+++ src/main/webapp/js/submenu.js
@@ -0,0 +1,10 @@
+$(function(){
+	var url = window.location.href;
+	
+	$($('.nav-item').find('.button').get().reverse()).each(function(i, item){
+		if(url.indexOf(item) >= 0) {
+			$(item).css("background-color", "#F90");
+			return false;
+		}
+	});
+});
