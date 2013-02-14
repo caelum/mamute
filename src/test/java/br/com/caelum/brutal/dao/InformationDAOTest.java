b9534f2fad616969c7ac803d7ce7c62548adb52f
leonardowolter <leocwolter@gmail.com>
2013-02-14 17:10:29 -0200

    arrumando testes

--- src/test/java/br/com/caelum/brutal/dao/InformationDAOTest.java
+++ src/test/java/br/com/caelum/brutal/dao/InformationDAOTest.java
@@ -6,7 +6,10 @@ import java.util.List;
 
 import org.junit.Test;
 
+import br.com.caelum.brutal.model.Answer;
+import br.com.caelum.brutal.model.AnswerInformation;
 import br.com.caelum.brutal.model.Information;
+import br.com.caelum.brutal.model.LoggedUser;
 import br.com.caelum.brutal.model.Question;
 import br.com.caelum.brutal.model.QuestionInformation;
 import br.com.caelum.brutal.model.QuestionInformationBuilder;
@@ -15,11 +18,42 @@ import br.com.caelum.brutal.model.UpdateStatus;
 import br.com.caelum.brutal.model.User;
 import br.com.caelum.brutal.model.interfaces.Updatable;
 
-public class QuestionInformationDAOTest extends DatabaseTestCase {
+public class InformationDAOTest extends DatabaseTestCase {
+
+    @Test
+    public void should_find_pending_answers_edits() {
+        InformationDAO informations = new InformationDAO(session);
+        
+        User author = new User("namename", "email@gmail.com", "123456");
+        LoggedUser currentAuthor = new LoggedUser(author, null);
+        
+        QuestionInformation qinfo1 = new QuestionInformationBuilder().with(author).build();
+        Question question = new Question(qinfo1, author);
+        AnswerInformation info1 = new AnswerInformation("info1 info1 info1 info1 info1 info1 info1 ", currentAuthor, "");
+        Answer answer1 = new Answer(info1, question, author);
+        
+        AnswerInformation edit1 = new AnswerInformation("info2 info2 info2 info2 info2 info2 info2 ", currentAuthor, answer1, "");
+        edit1.setInitStatus(UpdateStatus.PENDING);
+        AnswerInformation edit2 = new AnswerInformation("info3 info3 info3 info3 info3 info3 info3 ", currentAuthor, answer1, "");
+        edit2.setInitStatus(UpdateStatus.PENDING);
+        
+        session.save(author);
+        session.save(question);
+        session.save(qinfo1);
+        session.save(info1);
+        session.save(answer1);
+        session.save(edit1);
+        session.save(edit2);
+        
+        UpdatablesAndPendingHistory pendingByUpdatables = informations.pendingByUpdatables(Answer.class);
+        
+        List<Information> pendingInfoForAnswer1 = pendingByUpdatables.pendingInfoFor(answer1);
+        assertEquals(2, pendingInfoForAnswer1.size());
+    }
 
     @Test
     public void should_get_pending_history() {
-        QuestionInformationDAO questionInformations = new QuestionInformationDAO(session);
+        InformationDAO informations = new InformationDAO(session);
         User author = new User("francisco", "chico@chico", "123456");
         
         QuestionInformation info1 = new QuestionInformationBuilder().with(author).build();
@@ -46,7 +80,7 @@ public class QuestionInformationDAOTest extends DatabaseTestCase {
         session.save(question2);
         session.save(question3);
         
-        UpdatablesAndPendingHistory pending = questionInformations.pendingByUpdatables();
+        UpdatablesAndPendingHistory pending = informations.pendingByUpdatables(Question.class);
         List<Updatable> questions = pending.questions();
         
         assertEquals(2, questions.size());
@@ -58,5 +92,4 @@ public class QuestionInformationDAOTest extends DatabaseTestCase {
         List<Information> pendingQuestion3 = pending.pendingInfoFor(questions.get(1));
         assertEquals(3, pendingQuestion3.size());
     }
-
 }
