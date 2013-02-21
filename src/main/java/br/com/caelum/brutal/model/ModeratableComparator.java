0c8258c7938af0aef6217826758832cc73a86409
Francisco Sokol <chico.sokol@gmail.com>
2013-02-21 13:14:45 -0300

    refatorando classe para usar moderatable ao inves de updatable

---
+++ src/main/java/br/com/caelum/brutal/model/ModeratableComparator.java
@@ -0,0 +1,16 @@
+package br.com.caelum.brutal.model;
+
+import java.util.Comparator;
+
+import br.com.caelum.brutal.model.interfaces.Moderatable;
+
+public class ModeratableComparator implements Comparator<Moderatable> {
+
+    @Override
+    public int compare(Moderatable moderatable1, Moderatable moderatable2) {
+        if (moderatable1.getId().equals(moderatable2.getId()))
+            return 0;
+        return (Long) moderatable1.getId() - (Long) moderatable2.getId() < 0l ? -1 : 1;
+    }
+
+}
