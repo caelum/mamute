b3469b001491c7d59c5c120aba5563fca2820179
leonardowolter <leocwolter@gmail.com>
2013-02-22 17:51:04 -0300

    indo direto pra answer no ajax também

--- src/main/webapp/js/order-by.js
+++ src/main/webapp/js/order-by.js
@@ -6,13 +6,20 @@ $(function(){
 			var listElements = "";
 			$(list).each(function(index, item){
 				var question = getQuestion(self.data("type"), item),
-					href = "/questions/"+question.id+"/"+question.information.sluggedTitle;
-				
+					href = getHref(self.data("type"), question, item);
 				listElements += "<li><span>"+item.voteCount+"</span> <a href='"+href+"'>"+question.information.title+"</a></li>";
 			});
 			$("#"+self.data("target-id")).html(listElements);
 		});
 	});
+	
+	function getHref(type, question, item){
+		var answerAnchor;
+		if(type == "answers"){
+			answerAnchor = "#answer-"+item.id;
+		}
+		return "/questions/"+question.id+"/"+question.information.sluggedTitle+answerAnchor;
+	}
 	
 	function getQuestion(type, item){
 		if(type == "questions"){
