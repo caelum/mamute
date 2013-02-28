e42ddbfdae21aeea88325691ae83dabc7f9bf60e
leonardowolter <leocwolter@gmail.com>
2013-02-28 13:43:05 -0300

    refatoracaozinha no popup e link-behaviour para nao deixar autor performar algo

--- src/main/webapp/js/links-behaviour.js
+++ src/main/webapp/js/links-behaviour.js
@@ -1,12 +1,23 @@
 $(function() {
-	$('.requires-karma').each(function(i, el) {
-		var x = $(el);
-		var required = parseInt(x.data("karma"));
-		if(!MODERATOR && required > KARMA && x.data("author")=="false") {
-			x.off('click');
-			x.click(function(e) {
+	$('.requires-karma').each(function(index, element) {
+		var element = $(element);
+		var required = parseInt(element.data("karma"));
+		if(!MODERATOR && required > KARMA && element.data("author")=="false") {
+			element.off('click');
+			element.click(function(e) {
 				e.preventDefault();
 				errorDiv("Você precisa ter "+ required +" karma!", this);
+			});
+		}
+	});
+	$('.author-cant').each(function(index, element) {
+		var element = $(element);
+		var isAuthor = element.data("author");
+		if(isAuthor) {
+			element.off('click');
+			element.click(function(e) {
+				e.preventDefault();
+				errorDiv("O autor não pode performar esta operação!", this);
 			});
 		}
 	});
@@ -26,11 +37,8 @@ $(function() {
 	
 	function errorDiv(text, self){
 		if($(".validation-error.popup").length == 0){
-			var errorDiv = $("<div class='validation-error popup'>"+text+"</div>");
-			errorDiv.appendTo($(self).parent()).show();
-			$(errorDiv).click(function(){
-				$(this).remove();
-			});
+			var errorDiv = $("<div class='validation-error popup close-popup'>"+text+"</div>");
+			errorDiv.insertAfter(self).show();
 		}
 	}
 });
