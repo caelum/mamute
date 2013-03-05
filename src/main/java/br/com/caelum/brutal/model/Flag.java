a6189bbf5910cc7eae3a2fa6c0c838048aa274ba
Francisco Sokol <chico.sokol@gmail.com>
2013-03-05 16:31:03 -0300

    adicionando tipo da flag

--- src/main/java/br/com/caelum/brutal/model/Flag.java
+++ src/main/java/br/com/caelum/brutal/model/Flag.java
@@ -1,6 +1,8 @@
 package br.com.caelum.brutal.model;
 
 import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
 import javax.persistence.GeneratedValue;
 import javax.persistence.Id;
 import javax.persistence.ManyToOne;
@@ -19,5 +21,8 @@ public class Flag {
 	
 	@ManyToOne
 	private User author;
-
+	
+	@Enumerated(EnumType.STRING)
+	private FlagType type;
+	
 }
