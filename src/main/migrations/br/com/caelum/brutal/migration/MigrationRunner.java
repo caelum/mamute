d7cddd1e1c349a31bfd9df4d78b9b4ec246bc86f
Francisco Sokol <chico.sokol@gmail.com>
2013-03-06 18:01:24 -0300

    não executando migrations em testing ou acceptance

--- src/main/migrations/br/com/caelum/brutal/migration/MigrationRunner.java
+++ src/main/migrations/br/com/caelum/brutal/migration/MigrationRunner.java
@@ -8,6 +8,7 @@ import javax.annotation.PostConstruct;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import br.com.caelum.vraptor.environment.Environment;
 import br.com.caelum.vraptor.ioc.ApplicationScoped;
 import br.com.caelum.vraptor.ioc.Component;
 
@@ -20,11 +21,13 @@ public class MigrationRunner {
 	private final static Logger LOGGER = LoggerFactory
 			.getLogger(MigrationRunner.class);
 	private final MigrationExecutor executor;
+	private final Environment env;
 
 	public MigrationRunner(List<Migration> migrations, NumberExtractor number,
-			MigrationExecutor executor) {
+			MigrationExecutor executor, Environment env) {
 		this.extractNumber = number;
 		this.executor = executor;
+		this.env = env;
 		this.migrations = sort(migrations);
 	}
 
@@ -36,6 +39,9 @@ public class MigrationRunner {
 
 	@PostConstruct 
 	public void execute() {
+		if (env.getName().equals("testing") || env.getName().equals("acceptance")) {
+			return;
+		}
 
 		prepareTables();
 
