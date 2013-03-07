235ba362827a48765c7b9941c2691040cde04108
Francisco Sokol <chico.sokol@gmail.com>
2013-03-07 19:32:55 -0300

    adicionando scribe e pegando token do usuário

--- src/main/java/br/com/caelum/brutal/auth/FacebookAuthService.java
+++ src/main/java/br/com/caelum/brutal/auth/FacebookAuthService.java
@@ -2,18 +2,34 @@ package br.com.caelum.brutal.auth;
 
 import java.util.Random;
 
+import org.scribe.builder.ServiceBuilder;
+import org.scribe.builder.api.FacebookApi;
+import org.scribe.model.Token;
+import org.scribe.model.Verifier;
+import org.scribe.oauth.OAuthService;
+
 import br.com.caelum.vraptor.environment.Environment;
 import br.com.caelum.vraptor.ioc.Component;
 
 @Component
 public class FacebookAuthService {
 	
-	private String redirectUri;
-	private String clientId;
+	private static final Token EMPTY_TOKEN = null;
+	private final String redirectUri;
+	private final String clientId;
+	private final String appSecret;
+	private OAuthService service;
 
 	public FacebookAuthService(Environment env) {
-		clientId = env.get("facebook.client_id");
-		redirectUri = env.get("facebook.redirect_uri");
+		this.clientId = env.get("facebook.client_id");
+		this.redirectUri = env.get("facebook.redirect_uri");
+		this.appSecret = env.get("facebook.app_secret");
+		this.service = new ServiceBuilder()
+			.provider(FacebookApi.class)
+			.apiKey(clientId)
+			.apiSecret(appSecret)
+			.callback(redirectUri)
+			.build();
 	}
 	
 	public String getOauthUrl() {
@@ -26,7 +42,9 @@ public class FacebookAuthService {
 	}
 
 	public void buildToken(String code) {
-		System.out.println(code);
+		Token accessToken = service.getAccessToken(EMPTY_TOKEN, new Verifier(code));
+		System.out.println("gotcha! " + accessToken);
+		throw new UnsupportedOperationException("do something with it: " + accessToken);
 	}
 	
 }
