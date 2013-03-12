46d3407a031aee96893f9dbf63d40709545ad7db
Francisco Sokol <chico.sokol@gmail.com>
2013-03-12 17:53:38 -0300

    comitando transação só se estiver ativa

--- src/main/java/br/com/caelum/brutal/providers/TransactionInterceptor.java
+++ src/main/java/br/com/caelum/brutal/providers/TransactionInterceptor.java
@@ -1,19 +1,39 @@
 package br.com.caelum.brutal.providers;
 
 import org.hibernate.Session;
+import org.hibernate.Transaction;
 
 import br.com.caelum.vraptor.Intercepts;
 import br.com.caelum.vraptor.Validator;
+import br.com.caelum.vraptor.core.InterceptorStack;
+import br.com.caelum.vraptor.interceptor.Interceptor;
 import br.com.caelum.vraptor.resource.ResourceMethod;
-import br.com.caelum.vraptor.util.hibernate.HibernateTransactionInterceptor;
 
 @Intercepts
-public class TransactionInterceptor extends HibernateTransactionInterceptor {
+public class TransactionInterceptor implements Interceptor {
 
+	private final Session session;
+	private final Validator validator;
 
 	public TransactionInterceptor(Session session, Validator validator) {
-        super(session, validator);
+		this.session = session;
+		this.validator = validator;
     }
+	
+	public void intercept(InterceptorStack stack, ResourceMethod method, Object instance) {
+		Transaction transaction = null;
+		try {
+			transaction = session.beginTransaction();
+			stack.next(method, instance);
+			if (!validator.hasErrors() && transaction.isActive()) {
+				transaction.commit();
+			}
+		} finally {
+			if (transaction != null && transaction.isActive()) {
+				transaction.rollback();
+			}
+		}
+	}
 
 	/**
 	 * to avoid misterious bugs, every transaction should be inside a transaction, either get or post
