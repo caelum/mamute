ffc904426372adbfae59ebc8d85b93818f629b2f
Francisco Sokol <chico.sokol@gmail.com>
2013-03-15 18:26:28 -0300

    pegando context path e colocando no ajax

--- src/main/java/br/com/caelum/brutal/providers/GlobalInterceptor.java
+++ src/main/java/br/com/caelum/brutal/providers/GlobalInterceptor.java
@@ -1,5 +1,6 @@
 package br.com.caelum.brutal.providers;
 
+import javax.servlet.ServletContext;
 import javax.servlet.http.HttpServletRequest;
 
 import org.joda.time.format.DateTimeFormat;
@@ -26,13 +27,16 @@ public class GlobalInterceptor implements Interceptor{
 	private final Access access;
 	private final HttpServletRequest req;
 	private final Localization localization;
+	private final ServletContext servletContext;
 
-	public GlobalInterceptor(Environment env, Result result, Access access, HttpServletRequest req, Localization localization) {
+	public GlobalInterceptor(Environment env, Result result, Access access, 
+			HttpServletRequest req, Localization localization, ServletContext servletContext) {
 		this.env = env;
 		this.result = result;
 		this.access = access;
 		this.req = req;
 		this.localization = localization;
+		this.servletContext = servletContext;
 	}
 
 	public void intercept(InterceptorStack stack, ResourceMethod method,
@@ -42,6 +46,7 @@ public class GlobalInterceptor implements Interceptor{
 		result.include("prettyTimeFormatter", new PrettyTime(localization.getLocale()));
 		result.include("literalFormatter", DateTimeFormat.forPattern(localization.getMessage("date.joda.pattern")).withLocale(localization.getLocale()));
 		result.include("currentUrl", req.getRequestURL());
+		result.include("contextPath", req.getContextPath());
 		stack.next(method, resourceInstance);
 	}
 
