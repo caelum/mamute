c932faa17e3f39856fe32b11d58e664080d6c8f3
leonardowolter <leocwolter@gmail.com>
2013-03-18 11:44:25 -0300

    anonimos nao podem editar

--- src/main/java/br/com/caelum/brutal/auth/rules/AuthorizationSystem.java
+++ src/main/java/br/com/caelum/brutal/auth/rules/AuthorizationSystem.java
@@ -17,13 +17,14 @@ public class AuthorizationSystem {
 	 * @throws UnauthorizedException if the user isn't allowed to edit this moderatable
 	 */
 	public boolean canEdit(Moderatable question, int karmaRequired) {
+		if(user == null) return false;
+
 		AuthorRule<Moderatable> isAuthor = new AuthorRule<Moderatable>();
 		PermissionRule<Moderatable> hasEnoughKarma = new MinimumKarmaRule<>(karmaRequired);
 		ModeratorRule<Moderatable> moderatorRule = new ModeratorRule<>();
 		ComposedRule<Moderatable> composed = new ComposedRule<>();
 		
 		ComposedRule<Moderatable> rule = composed.thiz(isAuthor).or(hasEnoughKarma).or(moderatorRule).or(moderatorRule);
-		
 		if (rule.isAllowed(user, question))
 			return true;
 		throw new UnauthorizedException("you are not the author or don't have enough karma"); // i18n here?
