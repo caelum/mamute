1c74d249f51357f141c0d84a6d438b58ffb808da
Francisco Sokol <chico.sokol@gmail.com>
2013-03-28 18:21:47 -0300

    log

--- src/main/java/br/com/caelum/brutal/notification/NotificationMailerJob.java
+++ src/main/java/br/com/caelum/brutal/notification/NotificationMailerJob.java
@@ -2,12 +2,14 @@ package br.com.caelum.brutal.notification;
 
 import java.util.List;
 
+import org.apache.log4j.Logger;
 import org.joda.time.DateTime;
 
 import br.com.caelum.brutal.dao.AnswerDAO;
 import br.com.caelum.brutal.dao.CommentDAO;
 import br.com.caelum.brutal.model.SubscribableDTO;
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.environment.Environment;
 import br.com.caelum.vraptor.quartzjob.CronTask;
 
@@ -18,22 +20,27 @@ public class NotificationMailerJob implements CronTask {
     private final CommentDAO comments;
     private final NotificationMailer notificationMailer;
 	private final Environment env;
+	private static final Logger LOG = Logger.getLogger(NotificationMailerJob.class);
+	private final Result result;
 
-    public NotificationMailerJob(AnswerDAO answers, CommentDAO comments, NotificationMailer notificationMailer, Environment env) {
+    public NotificationMailerJob(AnswerDAO answers, CommentDAO comments, NotificationMailer notificationMailer, Environment env, Result result) {
         this.answers = answers;
         this.comments = comments;
         this.notificationMailer = notificationMailer;
 		this.env = env;
+		this.result = result;
     }
 
     @Override
     public void execute() {
+    	LOG.info("executing NotificationMailerJob...");
         DateTime threeHoursAgo = new DateTime().minusHours(3);
         
         List<SubscribableDTO> recentSubscribables = answers.getSubscribablesAfter(threeHoursAgo);
         recentSubscribables.addAll(comments.getSubscribablesAfter(threeHoursAgo));
         
         notificationMailer.sendMails(recentSubscribables);
+        result.nothing();
     }
 
     @Override
