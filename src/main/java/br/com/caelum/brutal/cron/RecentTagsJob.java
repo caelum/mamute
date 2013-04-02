7659e0d2d99ae82f0da57453770a97528ede4bb7
Francisco Sokol <chico.sokol@gmail.com>
2013-04-02 13:56:14 -0300

    adding logs and result.nothing

--- src/main/java/br/com/caelum/brutal/cron/RecentTagsJob.java
+++ src/main/java/br/com/caelum/brutal/cron/RecentTagsJob.java
@@ -1,25 +1,32 @@
 package br.com.caelum.brutal.cron;
 
+import org.apache.log4j.Logger;
 import org.hibernate.Session;
 
 import br.com.caelum.brutal.components.RecentTagsContainer;
 import br.com.caelum.vraptor.Resource;
+import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.quartzjob.CronTask;
 
 @Resource
-public class RecentTagsJob implements CronTask{
+public class RecentTagsJob implements CronTask {
 	
-	private RecentTagsContainer recentTagsContainer;
+	private final RecentTagsContainer recentTagsContainer;
     private final Session session;
+	private final Result result;
+	private final static Logger LOG = Logger.getLogger(Logger.class);
 
-	public RecentTagsJob(RecentTagsContainer recentTagsContainer, Session session) {
+	public RecentTagsJob(RecentTagsContainer recentTagsContainer, Session session, Result result) {
 		this.recentTagsContainer = recentTagsContainer;
         this.session = session;
+		this.result = result;
 	}
 
 	@Override
 	public void execute() {
+		LOG.info("executing RecentTagsJob...");
 		recentTagsContainer.update(session);
+		result.nothing();
 	}
 
 	@Override
