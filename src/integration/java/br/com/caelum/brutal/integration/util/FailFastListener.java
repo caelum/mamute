5ec399063fb42f3ce21904c7a928fb26110941fe
Francisco Sokol <chico.sokol@gmail.com>
2013-04-08 17:37:22 -0300

    environment para ver se os testes de integração devem falhar

--- src/integration/java/br/com/caelum/brutal/integration/util/FailFastListener.java
+++ src/integration/java/br/com/caelum/brutal/integration/util/FailFastListener.java
@@ -1,28 +1,22 @@
 package br.com.caelum.brutal.integration.util;
 
-import java.io.File;
-
-import org.apache.commons.io.FileUtils;
 import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunListener;
-import org.openqa.selenium.WebDriver;
 
 import br.com.caelum.brutal.integration.scene.AcceptanceTestBase;
 
 public class FailFastListener extends RunListener {
-	
+
 	public void testFailure(Failure failure) throws Exception {
-		System.err.println("FAILURE: " + failure);
-		String getenv = System.getenv("CLOSE_DRIVER_ON_EXIT");
-		WebDriver driver = AcceptanceTestBase.getDriver();
-		String pageSource = driver.getPageSource();
-		File file = new File("/tmp/error.html");
-		System.err.println("page source written to " + file.getAbsolutePath());
-		FileUtils.writeStringToFile(file, pageSource);
-		if (getenv != null) {
-			AcceptanceTestBase.close();
+		String failFast = System.getenv("FAIL_FAST");
+		if ("true".equals(failFast)) {
+			System.err.println("FAILURE: " + failure);
+			Boolean closeOnExit = System.getenv("CLOSE_DRIVER_ON_EXIT") != null;
+			if (closeOnExit) {
+				AcceptanceTestBase.close();
+			}
+			System.exit(-1);
 		}
-		System.exit(-1);
 	}
-	
+
 }
