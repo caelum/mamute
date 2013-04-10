6b4e0a74ba4a93fcdefb138e801d55d52d56a64c
Francisco Sokol <chico.sokol@gmail.com>
2013-04-10 12:53:44 -0300

    imprimindo stacktrace no failfast

--- src/integration/java/br/com/caelum/brutal/integration/util/FailFastListener.java
+++ src/integration/java/br/com/caelum/brutal/integration/util/FailFastListener.java
@@ -11,6 +11,7 @@ public class FailFastListener extends RunListener {
 		String failFast = System.getenv("FAIL_FAST");
 		if ("true".equals(failFast)) {
 			System.err.println("FAILURE: " + failure);
+			failure.getException().printStackTrace();
 			Boolean closeOnExit = System.getenv("CLOSE_DRIVER_ON_EXIT") != null;
 			if (closeOnExit) {
 				AcceptanceTestBase.close();
