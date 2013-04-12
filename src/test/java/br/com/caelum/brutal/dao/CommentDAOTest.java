e5747fae18e29668edb6924e860e0ba6b4613d56
Artur Diniz Adam <artur.daz@gmail.com>
2013-04-12 18:52:41 -0300

    testando notificacao de comments(not funfing)

--- src/test/java/br/com/caelum/brutal/dao/CommentDAOTest.java
+++ src/test/java/br/com/caelum/brutal/dao/CommentDAOTest.java
@@ -82,56 +82,66 @@ public class CommentDAOTest extends DatabaseTestCase {
         
         assertEquals(beberFazMal.getId(), recentComments.get(0).getQuestion().getId());
     }
-    
-        
 
     @Test
-    public void should_not_find_unsubscribed_users() {
-        User artur = user("question author", "qauthor@gmail");
+    public void should_not_find_unsubscribed_and_find_subscribed_users_for_comments() {
+        User artur = user("question author", "artur@gmail.com");
         artur.setSubscribed(false);
-        User iFag = user("question author", "otherqauthor@gmail");
-        User valeriano = user("answer author", "aauthor@gmail");
+        
+        User valeriano = user("answer author", "valeriano@gmail.com");
         valeriano.setSubscribed(false);
-        User alcoolatra = user("comment author", "cauthor@gmail");
-        Tag tagDefault = tag("default");
-        session.save(tagDefault);
-        QuestionBuilder question = new QuestionBuilder();        
-
-		Question beberFazMal = question
-			.withTitle("Por que dizem que beber demais faz mal?")
-			.withDescription("Alguem poderia me dizer o por que disso? Obrigado galera!")
-			.withAuthor(artur)
-			.withTag(tagDefault)
-			.build();
-		
-        Answer figadoVaiProSaco = answer("Por que seu figado vai pro saco", beberFazMal, valeriano);
         
-        Question androidRuim = question
-				.withTitle("Por que a api de android é tão ruim?")
-				.withDescription("Alguem poderia me dizer o por que disso? Obrigado galera!")
-				.withAuthor(iFag)
-				.withTag(tagDefault)
-				.build();
+        User iFag = user("question author", "ifag@gmail.com");
+        User alcoolatra = user("comment author", "alcoolatra@gmail.com");
         
-        Answer naoEhRuby = answer("Por que não é ruby, ai não é bacana.", androidRuim, valeriano);
-        
-        Comment naoFazMal = new Comment(alcoolatra, "O que te levou a pensar que faz mal? Faz nada não.");
-        beberFazMal.add(naoFazMal);
+        User leo = user("question author", "leo@gmail.com");
+        User chico = user("comment author", "chico@gmail.com");
         
         session.save(artur);
         session.save(valeriano);
         session.save(alcoolatra);
         session.save(iFag);
+        session.save(leo);
+        session.save(chico);
+        
+        Tag tagDefault = tag("java");
+        session.save(tagDefault);
+        
+        List<User> users = new ArrayList<>();
+        
+        //comment added to question but question and answer author should not be notified
+		Question beberFazMal = question(artur, Arrays.asList(tagDefault));
+        Answer figadoVaiProSaco = answer("Por que seu figado vai pro saco", beberFazMal, valeriano);
+        Comment naoFazMal = new Comment(alcoolatra, "O que te levou a pensar que faz mal? Faz nada não.");
+        beberFazMal.add(naoFazMal);
         session.save(naoFazMal);
         session.save(beberFazMal);
         session.save(figadoVaiProSaco);
+        
+        //comment added to answer (question and answer author must be notified)
+        Question androidRuim = question(iFag, Arrays.asList(tagDefault));
+        Answer naoEhRuby = answer("Por que não é ruby, ai não é bacana.", androidRuim, alcoolatra);
+        Comment naoEhNao = comment(valeriano, "nao eh ruim nao");
+        naoEhRuby.add(naoEhNao);
+        session.save(naoEhNao);
         session.save(androidRuim);
         session.save(naoEhRuby);
         
+        //comment added to question (question and answer authors must be notified)
+        Question comoFaz = question(leo, Arrays.asList(tagDefault));
+        Answer fazAssim = answer("faz assim faz assim faz assim faz assim faz assim", comoFaz, chico);
+        Comment fazerOQue = comment(valeriano, "quer fazer o que?");
+        comoFaz.add(fazerOQue);
+        session.save(fazerOQue);
+        session.save(comoFaz);
+        session.save(fazAssim);
+        
         List<SubscribableDTO> recentComments = comments.getSubscribablesAfter(new DateTime().minusHours(3));
-        
-        assertEquals(0, recentComments.size());
-        
+        for (SubscribableDTO subscribableDTO : recentComments) {
+			users.add(subscribableDTO.getUser());
+		}
+        assertThat(users, not(containsInAnyOrder(artur, valeriano)));
+        assertThat(users, containsInAnyOrder(alcoolatra, iFag, leo, chico));
     }
     
     @Test
