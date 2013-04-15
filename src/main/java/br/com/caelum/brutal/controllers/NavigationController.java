39849be0544ee1b51e0a2aa3ec064f9ee467f2ff
Francisco Sokol <chico.sokol@gmail.com>
2013-04-15 12:16:29 -0300

    imports

--- src/main/java/br/com/caelum/brutal/controllers/NavigationController.java
+++ src/main/java/br/com/caelum/brutal/controllers/NavigationController.java
@@ -1,12 +1,5 @@
 package br.com.caelum.brutal.controllers;
 
-import java.lang.reflect.Field;
-import java.util.List;
-
-import net.vidageek.mirror.dsl.ClassController;
-import net.vidageek.mirror.dsl.Mirror;
-import br.com.caelum.brutal.auth.rules.PermissionRulesConstants;
-import br.com.caelum.brutal.reputation.rules.KarmaCalculator;
 import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Resource;
 
