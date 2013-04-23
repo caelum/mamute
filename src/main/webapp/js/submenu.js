f8e4e556e5528e2aefd6486eea9c357d75cc6284
leonardowolter <leocwolter@gmail.com>
2013-04-23 18:13:51 -0300

    voltando a funcionar marcacao do header

--- src/main/webapp/js/submenu.js
+++ src/main/webapp/js/submenu.js
@@ -1,6 +1,13 @@
 $(function(){
 	var url = window.location.href;
 	
+	$($('.nav-item').find('.button').get().reverse()).each(function(i, item){
+		if(url.indexOf(item) >= 0) {
+			$(item).addClass("current");
+			return false;
+		}
+	});
+	
 	$($('.tabs').find('a').get().reverse()).each(function(i, item){
 		if(url.indexOf(item) >= 0) {
 			$(item).addClass("selected");
