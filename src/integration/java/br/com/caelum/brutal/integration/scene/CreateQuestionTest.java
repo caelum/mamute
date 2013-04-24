0b02132583a7e4285e4d5922aedeb66a06fd745e
Francisco Sokol <chico.sokol@gmail.com>
2013-04-24 18:59:45 -0300

    fazendo logout direto na url

--- src/integration/java/br/com/caelum/brutal/integration/scene/CreateQuestionTest.java
+++ src/integration/java/br/com/caelum/brutal/integration/scene/CreateQuestionTest.java
@@ -1,7 +1,7 @@
 package br.com.caelum.brutal.integration.scene;
 
-import static junit.framework.Assert.assertTrue;
 import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
 import static org.openqa.selenium.By.className;
 
 import org.junit.Before;
