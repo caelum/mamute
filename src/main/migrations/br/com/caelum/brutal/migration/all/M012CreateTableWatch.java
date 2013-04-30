e1c7f8157ebbdbe64876d5fee3c3c8375057221f
leonardowolter <leocwolter@gmail.com>
2013-04-30 18:10:18 -0300

    migration de watch

---
+++ src/main/migrations/br/com/caelum/brutal/migration/all/M012CreateTableWatch.java
@@ -0,0 +1,30 @@
+package br.com.caelum.brutal.migration.all;
+
+import java.util.List;
+
+import br.com.caelum.brutal.migration.Migration;
+import br.com.caelum.brutal.migration.MigrationOperation;
+import br.com.caelum.brutal.migration.RawSQLOperation;
+import br.com.caelum.vraptor.ioc.ApplicationScoped;
+import br.com.caelum.vraptor.ioc.Component;
+
+@Component
+@ApplicationScoped
+public class M012CreateTableWatch implements Migration {
+
+	@Override
+	public List<MigrationOperation> up() {
+		String createSql = "create table Watch (id bigint not null auto_increment, active bit not null, watchedQuestion_id bigint, watcher_id bigint, primary key (id)) ENGINE=InnoDB";
+		String createUserForeignKey = "alter table Watch add index FK4F7D4AF8780E8FC (watcher_id), add constraint FK4F7D4AF8780E8FC foreign key (watcher_id) references Users (id)";
+		String createQuestionForeignKey = "alter table Watch add index FK4F7D4AF9A1A0ABF (watchedQuestion_id), add constraint FK4F7D4AF9A1A0ABF foreign key (watchedQuestion_id) references Question (id)"; 
+		List<MigrationOperation> ops = RawSQLOperation.forSqls(createSql, createUserForeignKey, createQuestionForeignKey);
+		return ops;
+	}
+
+	@Override
+	public List<MigrationOperation> down() {
+		String dropSql = "drop table Watch";
+		return RawSQLOperation.forSqls(dropSql);
+	}
+
+}
