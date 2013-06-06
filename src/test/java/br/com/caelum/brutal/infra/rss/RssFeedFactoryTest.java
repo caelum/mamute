03c3831c7b26246afe418bdb51afed62ea180515
Artur Diniz Adam <artur.daz@gmail.com>
2013-06-06 17:26:42 -0300

    Arrumando teste do rss factory

--- src/test/java/br/com/caelum/brutal/infra/rss/RssFeedFactoryTest.java
+++ src/test/java/br/com/caelum/brutal/infra/rss/RssFeedFactoryTest.java
@@ -1,6 +1,7 @@
 package br.com.caelum.brutal.infra.rss;
 
 import static java.util.Arrays.asList;
+import static org.junit.Assert.assertTrue;
 
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
@@ -11,6 +12,7 @@ import org.junit.Test;
 import br.com.caelum.brutal.builder.QuestionBuilder;
 import br.com.caelum.brutal.dao.TestCase;
 import br.com.caelum.brutal.model.Question;
+import br.com.caelum.brutal.model.User;
 
 public class RssFeedFactoryTest extends TestCase {
 	
@@ -21,23 +23,25 @@ public class RssFeedFactoryTest extends TestCase {
 		QuestionBuilder builder = new QuestionBuilder();
 		
 		DateTimeUtils.setCurrentMillisFixed(100);
-		Question question1 = builder.withAuthor(user("author1", "author@email"))
+		User user1 = user("author1", "author@email");
+		Question question1 = builder.withAuthor(user1)
 			.withTitle("first question")
 			.withDescription("question")
 			.withId(1l)
 			.build();
 		
+		User user2 = user("author2", "author@email");
 		Question question2 = builder.withId(2l)
 			.withTitle("second question")
-			.withAuthor(user("author2", "author@email"))
+			.withAuthor(user2)
 			.build();
 		
 		ByteArrayOutputStream output = new ByteArrayOutputStream();
 		rssFeedFactory.build(asList(question1, question2), output);
 		output.close();
 		String xml = new String(output.toByteArray());
-		xml.contains("first question");
-		xml.contains("second question");
+		assertTrue(xml.contains("first question"));
+		assertTrue(xml.contains("second question"));
 		DateTimeUtils.setCurrentMillisSystem();
 	}
 }
