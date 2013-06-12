4cd94dbcd38dd8d27452ee396d217e46c6df9129
Felipe Torres <felipe.gtorres@gmail.com>
2013-06-11 21:44:38 -0300

    Testes para o MassiveVote

--- src/main/java/br/com/caelum/brutal/model/MassiveVote.java
+++ src/main/java/br/com/caelum/brutal/model/MassiveVote.java
@@ -13,10 +13,13 @@ import br.com.caelum.vraptor.ioc.ApplicationScoped;
 @ApplicationScoped
 public class MassiveVote {
 	
+	private static final int MAX_VOTE_ALLOWED = 5;
+	private static final int MIN_DAY = 2;
+	
 	Map<User, List<Vote>> upvotesByTarget = new HashMap<>();
 	Map<User, List<Vote>> downvotesByTarget = new HashMap<>();
 
-	public boolean shouldCountKarma(User author, User target, Vote current){
+	public boolean shouldCountKarma(User author, User target, Vote current) {
 
 		Map<User, List<Vote>> votesByTarget;
 		if(current.getType().equals(VoteType.UP)) votesByTarget = upvotesByTarget;
@@ -28,9 +31,9 @@ public class MassiveVote {
 		
 		Collections.sort(votes, new VoteComparator());
 			
-		DateTime antonte = new DateTime().minusDays(2);
+		DateTime antonte = new DateTime().minusDays(MIN_DAY);
 			
-		if(votes.size() == 5) {
+		if(votes.size() == MAX_VOTE_ALLOWED) {
 			if(votes.get(0).getCreatedAt().isAfter(antonte)) {
 				return false;
 			}
@@ -39,11 +42,14 @@ public class MassiveVote {
 				votes.add(current);
 				return true;
 			}
-		} else {
+		} 
+		else if (votes.size() < MAX_VOTE_ALLOWED){
 			votes.add(current);
 			votesByTarget.put(target, votes);
 			return true;
 		}
+		
+		throw new IllegalArgumentException("massive vote size are bigger than MAX_VOTE_ALLOWED");
 	}
 	
 	private List<Vote> getVotesFrom(List<Vote> votes, User author) {
@@ -56,5 +62,9 @@ public class MassiveVote {
 		}
 		
 		return votesFromAuthor;
+	}
+
+	public int getMaxVotesAllowed() {
+		return MAX_VOTE_ALLOWED;
 	}
 }
