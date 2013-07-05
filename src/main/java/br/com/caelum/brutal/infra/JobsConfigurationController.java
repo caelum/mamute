89ab2f21676235dbac97675dac80b1b0da52dfd3
Francisco Sokol <chico.sokol@gmail.com>
2013-07-05 15:58:24 -0300

    log

--- src/main/java/br/com/caelum/brutal/infra/JobsConfigurationController.java
+++ src/main/java/br/com/caelum/brutal/infra/JobsConfigurationController.java
@@ -1,5 +1,6 @@
 package br.com.caelum.brutal.infra;
 
+import org.apache.log4j.Logger;
 import org.quartz.SchedulerException;
 
 import br.com.caelum.vraptor.Get;
@@ -10,22 +11,21 @@ import br.com.caelum.vraptor.quartzjob.QuartzController;
 @Resource
 public class JobsConfigurationController {
 	static final String CONFIG_PATH = "/configureJobs";
+	private static final Logger LOG = Logger.getLogger(JobsConfigurationController.class);
 	
 	private final QuartzController controller;
-
 	private final Result result;
 
 	public JobsConfigurationController(QuartzController controller, Result result) {
 		this.controller = controller;
 		this.result = result;
 	}
-	
 
 	@Get(CONFIG_PATH)
 	public void configure() throws SchedulerException {
+		LOG.debug("configuring cron jobs");
 		controller.config();
 		result.nothing();
 	}
-	
 
 }
