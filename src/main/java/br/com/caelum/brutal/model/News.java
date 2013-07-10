09c19814cff34fc978a039df8a76c8003fcf0714
leonardowolter <leocwolter@gmail.com>
2013-07-10 18:20:00 -0300

    comecando news

---
+++ src/main/java/br/com/caelum/brutal/model/News.java
@@ -0,0 +1,200 @@
+package br.com.caelum.brutal.model;
+
+import static javax.persistence.FetchType.EAGER;
+import static org.hibernate.annotations.CascadeType.SAVE_UPDATE;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.persistence.Embedded;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToOne;
+import javax.persistence.OneToMany;
+import javax.validation.constraints.NotNull;
+
+import org.hibernate.annotations.Cascade;
+import org.hibernate.annotations.Type;
+import org.joda.time.DateTime;
+
+import br.com.caelum.brutal.model.interfaces.Moderatable;
+import br.com.caelum.brutal.model.interfaces.Votable;
+
+@Entity
+public class News extends Moderatable implements Post {
+	@Id
+	@GeneratedValue
+	private Long id;
+
+	@ManyToOne(optional = false, fetch = EAGER)
+	@Cascade(SAVE_UPDATE)
+	@NotNull
+	private NewsInformation information = null;
+	
+	@OneToMany(mappedBy="news")
+	@Cascade(SAVE_UPDATE)
+	private List<NewsInformation> history = new ArrayList<>();
+	
+	@Type(type = "org.joda.time.contrib.hibernate.PersistentDateTime")
+	private final DateTime createdAt = new DateTime();
+
+	@Type(type = "org.joda.time.contrib.hibernate.PersistentDateTime")
+	private DateTime lastUpdatedAt = new DateTime();
+
+	@ManyToOne
+	private User lastTouchedBy = null;
+
+	@ManyToOne(fetch = EAGER)
+	private User author;
+
+	private long views = 0l;
+	
+	@JoinTable(name = "News_Votes")
+	@OneToMany
+	private final List<Vote> votes = new ArrayList<>();
+
+	private long voteCount = 0l;
+
+	@Embedded
+	private final NewsCommentList comments = new NewsCommentList();
+	
+	@Embedded
+	private final ModerationOptions moderationOptions = new ModerationOptions();
+	
+	@JoinTable(name = "Question_Flags")
+	@OneToMany
+	private final List<Flag> flags = new ArrayList<>();
+	
+	@Override
+	public void substitute(Vote previous, Vote current) {
+		this.voteCount += current.substitute(previous, votes);
+	}
+
+	@Override
+	public User getAuthor() {
+		return author;
+	}
+
+	@Override
+	public Long getId() {
+		return id;
+	}
+
+	@Override
+	public long getVoteCount() {
+		return voteCount;
+	}
+
+	@Override
+	public Class<? extends Votable> getType() {
+		return News.class;
+	}
+
+	@Override
+	public Comment add(Comment comment) {
+		comments.add(comment);
+		return comment;
+	}
+
+	@Override
+	public List<Comment> getVisibleCommentsFor(User user) {
+		return comments.getVisibleCommentsFor(user);
+	}
+
+	@Override
+	public DateTime getLastUpdatedAt() {
+		return lastUpdatedAt;
+	}
+
+	@Override
+	public User getLastTouchedBy() {
+		return lastTouchedBy;
+	}
+
+	@Override
+	public DateTime getCreatedAt() {
+		return createdAt;
+	}
+
+	@Override
+	public boolean isEdited() {
+		return history.size() > 1;
+	}
+
+	@Override
+	public boolean alreadyFlaggedBy(User user) {
+		for (Flag flag : flags) {
+			if (flag.createdBy(user))
+				return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void remove() {
+		moderationOptions.remove();
+	}
+
+	@Override
+	public boolean isVisible() {
+		return moderationOptions.isVisible();
+	}
+
+	@Override
+	public boolean isVisibleForModeratorAndNotAuthor(User user) {
+		return !this.isVisible() && user != null && !user.isAuthorOf(this);
+	}
+	
+	@Override
+	public String getTypeNameKey() {
+		return ""; //TODO TypeNameKey
+	}
+
+	@Override
+	protected NewsInformation getInformation() {
+		return information;
+	}
+
+	@Override
+	protected void updateApproved(Information approved) {
+		NewsInformation approvedQuestion = (NewsInformation) approved;
+		touchedBy(approvedQuestion.getAuthor());
+		setInformation(approvedQuestion);		
+	}
+
+	private void setInformation(NewsInformation approvedNews) {
+		approvedNews.setNews(this);
+        this.information = approvedNews;		
+	}
+
+	private void touchedBy(User author) {
+		this.lastTouchedBy = author;
+		this.lastUpdatedAt = new DateTime();
+	}
+
+	@Override
+	public String getTypeName() {
+        return getType().getSimpleName();
+    }
+	
+	@Override
+	public boolean hasPendingEdits() {
+		for (NewsInformation information : history) {
+			if(information.isPending()) return true;
+		}
+		return false;
+	}
+
+	@Override
+	public void add(Flag flag) {
+		flags.add(flag);
+	}
+
+	@Override
+	public Question getQuestion() {
+		return null; //TODO: remove getQuestion from Post interface
+	}
+
+}
