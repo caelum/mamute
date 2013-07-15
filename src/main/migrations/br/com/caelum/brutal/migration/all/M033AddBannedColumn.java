8fd4e7a6f2364ee1a4c121703540cba193b7405c
Artur Diniz Adam <artur.daz@gmail.com>
2013-07-15 18:46:20 -0300

    migration para o isBanned

---
+++ src/main/migrations/br/com/caelum/brutal/migration/all/M033AddBannedColumn.java
@@ -0,0 +1,16 @@
+package br.com.caelum.brutal.migration.all;
+
+import br.com.caelum.brutal.migration.SimpleMigration;
+import br.com.caelum.vraptor.ioc.ApplicationScoped;
+import br.com.caelum.vraptor.ioc.Component;
+
+@Component
+@ApplicationScoped
+public class M033AddBannedColumn extends SimpleMigration {
+
+	@Override
+	public String rawQuery() {
+		return "alter table Users add column banned bit(1) default 0";
+	}
+
+}
