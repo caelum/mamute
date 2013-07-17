aa64c1f9488b4562f10458bd2dacf0a45c2a206d
leonardowolter <leocwolter@gmail.com>
2013-07-17 18:57:04 -0300

    base para procurar por varios padroes de metodos mais bonitamente

--- src/main/java/br/com/caelum/brutal/brutauth/reflection/DefaultMethodInvoker.java
+++ src/main/java/br/com/caelum/brutal/brutauth/reflection/DefaultMethodInvoker.java
@@ -8,6 +8,12 @@ import br.com.caelum.vraptor.ioc.Component;
 @Component
 public class DefaultMethodInvoker {
 
+		private final BrutauthMethodSearchers searcher;
+
+		public DefaultMethodInvoker(BrutauthMethodSearchers searcher) {
+			this.searcher = searcher;
+		}
+	
 		public boolean invoke(CustomBrutauthRule toInvoke, Object[] args) {
 			BrutauthMethod brutauthMethod = getBrutalMethodWithParametersToInvoke(toInvoke, args);
 			return brutauthMethod.invoke();
