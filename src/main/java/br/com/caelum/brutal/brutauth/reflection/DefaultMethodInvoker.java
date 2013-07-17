682964fcc7483d4793582e2b683cd9b262afceda
leonardowolter <leocwolter@gmail.com>
2013-07-17 18:46:09 -0300

    match para poder usar apenas alguns parametros no isAllowed(ainda nao funciona)

--- src/main/java/br/com/caelum/brutal/brutauth/reflection/DefaultMethodInvoker.java
+++ src/main/java/br/com/caelum/brutal/brutauth/reflection/DefaultMethodInvoker.java
@@ -23,8 +23,17 @@ public class DefaultMethodInvoker {
 			}
 		}
 
-		private Object[] getArgumentsThatMatchToTypes(Class[] types, Object[] args) {
-			return args; //TODO Match args to types
+		private Object[] getArgumentsThatMatchToTypes(Class<?>[] types, Object[] args) {
+			Object[] argsToUse = new Object[types.length];
+			for (int i = 0; i < types.length; i++) {
+				for (Object arg : args) {
+					if(arg.getClass().isAssignableFrom(types[i])){
+						argsToUse[i] = arg;
+					}
+				}
+				if(argsToUse[i] == null) throw new IllegalArgumentException("O metodo do seu controller não recebe todos os argumentos que o isAllowed espera!");
+			}
+			return argsToUse;
 		}
 
 		private Method getMethod(String string, CustomBrutauthRule toInvoke) throws NoSuchMethodException {
@@ -35,10 +44,5 @@ public class DefaultMethodInvoker {
 			throw new NoSuchMethodException("Não existe o metodo default 'isAllowed' na classe: "+ toInvoke.getClass());
 		}
 
-		private Object[] fakeVarArgs() {
-			Object[] objects = new Object[1];
-			objects[0] = new Object[0];
-			return objects;
-		}
 
 }
