997a0d5a5a2db52352c2dddee9680348f10d31ad
leonardowolter <leocwolter@gmail.com>
2013-07-17 15:41:55 -0300

    algumas refatoraçoes, tirando o reflection do brutauauthcomposedrule

--- src/main/java/br/com/caelum/brutal/brutauth/interceptors/BrutalAuthInterceptor.java
+++ src/main/java/br/com/caelum/brutal/brutauth/interceptors/BrutalAuthInterceptor.java
@@ -1,10 +1,13 @@
 package br.com.caelum.brutal.brutauth.interceptors;
 
-import br.com.caelum.brutal.brutauth.auth.BrutauthComposedRule;
+import static br.com.caelum.vraptor.view.Results.http;
+import br.com.caelum.brutal.brutauth.auth.BrutauthReflectionComposedRule;
 import br.com.caelum.brutal.brutauth.auth.rules.CustomBrutauthRule;
 import br.com.caelum.brutal.brutauth.rules.AuthRules;
+import br.com.caelum.brutal.brutauth.util.DefaultMethodInvoker;
 import br.com.caelum.vraptor.InterceptionException;
 import br.com.caelum.vraptor.Intercepts;
+import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.core.InterceptorStack;
 import br.com.caelum.vraptor.core.MethodInfo;
 import br.com.caelum.vraptor.interceptor.ExecuteMethodInterceptor;
@@ -12,16 +15,21 @@ import br.com.caelum.vraptor.interceptor.Interceptor;
 import br.com.caelum.vraptor.interceptor.ParametersInstantiatorInterceptor;
 import br.com.caelum.vraptor.ioc.Container;
 import br.com.caelum.vraptor.resource.ResourceMethod;
+import br.com.caelum.vraptor.view.Results;
 
 @Intercepts(before=ExecuteMethodInterceptor.class, after=ParametersInstantiatorInterceptor.class)
 public class BrutalAuthInterceptor implements Interceptor{
 
 	private final Container container;
 	private final MethodInfo methodInfo;
+	private final DefaultMethodInvoker invoker;
+	private final Result result;
 
-	public BrutalAuthInterceptor(Container container, MethodInfo methodInfo) {
+	public BrutalAuthInterceptor(Container container, MethodInfo methodInfo, DefaultMethodInvoker invoker, Result result) {
 		this.container = container;
 		this.methodInfo = methodInfo;
+		this.invoker = invoker;
+		this.result = result;
 	}
 	
 	@Override
@@ -30,21 +38,24 @@ public class BrutalAuthInterceptor implements Interceptor{
 		AuthRules annotation = method.getMethod().getAnnotation(AuthRules.class);
 		Class<? extends CustomBrutauthRule>[] values = annotation.value();
 		
-		BrutauthComposedRule brutauthComposedRule = null;
+		BrutauthReflectionComposedRule brutauthComposedRule = null;
 		for (Class<? extends CustomBrutauthRule> value : values) {
 			CustomBrutauthRule brutauthRule = container.instanceFor(value);
 			brutauthComposedRule = compose(brutauthComposedRule, brutauthRule);
 		}
 		
 		boolean allowed = brutauthComposedRule.isAllowed(methodInfo.getParameters());
-		System.out.println(allowed);
+		if(!allowed){
+			result.use(http()).sendError(403);
+			return;
+		}
 		
 		stack.next(method, resourceInstance);
 	}
 
-	private BrutauthComposedRule compose(BrutauthComposedRule brutauthComposedRule, CustomBrutauthRule brutauthRule) {
+	private BrutauthReflectionComposedRule compose(BrutauthReflectionComposedRule brutauthComposedRule, CustomBrutauthRule brutauthRule) {
 		if(brutauthComposedRule == null){
-			brutauthComposedRule = new BrutauthComposedRule(brutauthRule);
+			brutauthComposedRule = new BrutauthReflectionComposedRule(brutauthRule, invoker);
 		}else{
 			brutauthComposedRule.and(brutauthRule);
 		}
