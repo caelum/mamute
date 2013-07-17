fd630d0a16e6044e0e538647e03c6b7eb1f8e356
leonardowolter <leocwolter@gmail.com>
2013-07-17 15:42:06 -0300

    user é @Nullable

--- src/main/java/br/com/caelum/brutal/brutauth/auth/rules/BrutauthAuthorRule.java
+++ src/main/java/br/com/caelum/brutal/brutauth/auth/rules/BrutauthAuthorRule.java
@@ -1,10 +1,7 @@
 package br.com.caelum.brutal.brutauth.auth.rules;
 
-<<<<<<< HEAD
 import javax.annotation.Nullable;
 
-=======
->>>>>>> criando anotação e interceptor para regras mais simples
 import br.com.caelum.brutal.model.Question;
 import br.com.caelum.brutal.model.User;
 import br.com.caelum.vraptor.ioc.Component;
@@ -14,19 +11,11 @@ public class BrutauthAuthorRule implements CustomBrutauthRule {
 
 	private final User user;
 
-<<<<<<< HEAD
 	public BrutauthAuthorRule(@Nullable User user) {
 		this.user = user;
 	}
 
 	public boolean isAllowed(Question question) {
-=======
-	public BrutauthAuthorRule(User user) {
-		this.user = user;
-	}
-	
-	public boolean isAllowed(Question question, String sluggedTitle) {
->>>>>>> criando anotação e interceptor para regras mais simples
 		if (user == null) return false;
 		if (question.getAuthor() == null) {
 			throw new IllegalArgumentException("can't verify permissions on item without an author");
