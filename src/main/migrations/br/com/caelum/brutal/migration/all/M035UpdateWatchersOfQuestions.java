74d3427c5b84a406f99add085efc28bc93b48d1c
leonardowolter <leocwolter@gmail.com>
2013-07-19 14:27:27 -0300

    dropando watcher se id da question for null

--- src/main/migrations/br/com/caelum/brutal/migration/all/M035UpdateWatchersOfQuestions.java
+++ src/main/migrations/br/com/caelum/brutal/migration/all/M035UpdateWatchersOfQuestions.java
@@ -30,10 +30,14 @@ public class M035UpdateWatchersOfQuestions  implements Migration {
 				List<Watcher> watchers = query.list();
 				for (Watcher watcher : watchers) {
 					BigInteger idOfQuestion = (BigInteger) session.createSQLQuery("select watchedQuestion_id from Watcher where id = "+watcher.getId()).uniqueResult();
-					Question question = (Question) session.createQuery("from Question where id = :id")
-							.setParameter("id", idOfQuestion.longValue())
-							.uniqueResult();
-					question.add(watcher);
+					if(idOfQuestion != null){
+						Question question = (Question) session.createQuery("from Question where id = :id")
+								.setParameter("id", idOfQuestion.longValue())
+								.uniqueResult();
+						question.add(watcher);
+					}else{
+						session.delete(watcher);
+					}
 				}
 			}
 
