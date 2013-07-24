2a9d7f4d39a61c4f8b897b414d289c66731fac2b
Francisco Sokol <chico.sokol@gmail.com>
2013-07-24 19:42:58 -0300

    @retation

--- src/main/java/br/com/caelum/brutal/brutauth/auth/annotations/HandledBy.java
+++ src/main/java/br/com/caelum/brutal/brutauth/auth/annotations/HandledBy.java
@@ -1,8 +1,12 @@
 package br.com.caelum.brutal.brutauth.auth.annotations;
 
+import java.lang.annotation.Retention;
+import java.lang.annotation.RetentionPolicy;
+
 import br.com.caelum.brutal.brutauth.auth.handlers.RuleHandler;
 
 
+@Retention(RetentionPolicy.RUNTIME)
 public @interface HandledBy {
 	Class<? extends RuleHandler> value();
 }
