be6bb9be5d9ffeebff185786d2d15fbf1a1d2c76
Francisco Sokol <chico.sokol@gmail.com>
2013-07-30 16:25:27 -0300

    arrumando equals

--- src/main/java/br/com/caelum/brutal/model/User.java
+++ src/main/java/br/com/caelum/brutal/model/User.java
@@ -358,7 +358,7 @@ public class User implements Identifiable {
 	public int hashCode() {
 		final int prime = 31;
 		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
+		result = prime * result + ((getId() == null) ? 0 : getId().hashCode());
 		return result;
 	}
 
@@ -368,13 +368,13 @@ public class User implements Identifiable {
 			return true;
 		if (obj == null)
 			return false;
-		if (getClass() != obj.getClass())
+		if (!(obj instanceof User))
 			return false;
 		User other = (User) obj;
-		if (id == null) {
-			if (other.id != null)
+		if (getId() == null) {
+			if (other.getId() != null)
 				return false;
-		} else if (!id.equals(other.id))
+		} else if (!getId().equals(other.getId()))
 			return false;
 		return true;
 	}
