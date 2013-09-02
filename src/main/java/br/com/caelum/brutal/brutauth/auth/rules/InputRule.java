b0d67de868ddb089dabe636dec880828a9fcfc50
leonardowolter <leocwolter@gmail.com>
2013-09-02 17:40:13 -0300

    desabilitando regra de input em acceptance

--- src/main/java/br/com/caelum/brutal/brutauth/auth/rules/InputRule.java
+++ src/main/java/br/com/caelum/brutal/brutauth/auth/rules/InputRule.java
@@ -9,6 +9,7 @@ import br.com.caelum.brutal.model.LoggedUser;
 import br.com.caelum.brutal.model.User;
 import br.com.caelum.brutauth.auth.annotations.HandledBy;
 import br.com.caelum.brutauth.auth.rules.CustomBrutauthRule;
+import br.com.caelum.vraptor.environment.Environment;
 
 @HandledBy(InputHandler.class)
 public class InputRule implements CustomBrutauthRule{
@@ -16,8 +17,10 @@ public class InputRule implements CustomBrutauthRule{
 	@Inject public LoggedUser user;
 	@Inject public InputManager input;
 	@Inject public HttpServletRequest request;
+	@Inject public Environment env;
 	
 	public boolean isAllowed(){
+		if("acceptance".equals(env.getName())) return true;
 		User current = user.getCurrent();
 		boolean isAllowed = input.can(current);
 		if(isAllowed && "POST".equals(request.getMethod())) input.ping(current);
