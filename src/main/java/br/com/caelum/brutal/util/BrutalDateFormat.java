8f10b35c3455142caa23c84a87e551041ad063b9
Fernanda Bernardo <fernandabernardo94@gmail.com>
2013-10-01 11:34:26 -0300

    trocando para bundleFormatter para funcionar data na newsletter

--- src/main/java/br/com/caelum/brutal/util/BrutalDateFormat.java
+++ src/main/java/br/com/caelum/brutal/util/BrutalDateFormat.java
@@ -1,19 +1,20 @@
 package br.com.caelum.brutal.util;
 
 import java.util.Locale;
-import java.util.ResourceBundle;
 
 import javax.inject.Inject;
 
 import org.joda.time.format.DateTimeFormat;
 import org.joda.time.format.DateTimeFormatter;
 
+import br.com.caelum.vraptor.simplemail.template.BundleFormatter;
+
 public class BrutalDateFormat {
 	
 	@Inject private Locale locale;
-	@Inject private ResourceBundle bundle;
+	@Inject private BundleFormatter bundle;
 	
 	public DateTimeFormatter getInstance (String pattern) {
-		return DateTimeFormat.forPattern(bundle.getString(pattern)).withLocale(locale);
+		return DateTimeFormat.forPattern(bundle.getMessage(pattern)).withLocale(locale);
 	}
 }
