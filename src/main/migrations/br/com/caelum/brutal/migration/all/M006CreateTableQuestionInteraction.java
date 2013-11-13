df223aee95158b49ad6b0253334ee3640f44bbac
Fernanda Bernardo <fernandabernardo94@gmail.com>
2013-11-13 19:20:09 -0200

    arrumando a relação de userInteraction para many to many

--- src/main/migrations/br/com/caelum/brutal/migration/all/M006CreateTableQuestionInteraction.java
+++ src/main/migrations/br/com/caelum/brutal/migration/all/M006CreateTableQuestionInteraction.java
@@ -12,9 +12,9 @@ import br.com.caelum.brutal.migration.SchemaMigration;
 public class M006CreateTableQuestionInteraction implements SchemaMigration {
 	@Override
 	public List<MigrationOperation> up() {
-		String createTable =  "create table Question_Interactions (Question_id bigint not null, userInteractions_id bigint not null ) ENGINE=InnoDB";
-		String alterTableFkUser = "alter table Question_Interactions add index FK_hrujl8eih9e72ve2xoddb36mm (userInteractions_id), add constraint FK_hrujl8eih9e72ve2xoddb36mm foreign key (userInteractions_id) references Users (id)"; 
-		String alterTableFkQuestion = "alter table Question_Interactions add index FK_chwtbfil7uh215g0yem6997k3 (Question_id), add constraint FK_chwtbfil7uh215g0yem6997k3 foreign key (Question_id) references Question (id)";  
+		String createTable =  "create table Question_Interactions (Question_id bigint not null, userInteractions_id bigint not null, PRIMARY KEY (`Question_id`,`userInteractions_id`)) ENGINE=InnoDB";
+		String alterTableFkUser = "alter table Question_Interactions add index FK_nydo4x8ey7gnhhwg1gqmikwo6 (userInteractions_id), add constraint FK_nydo4x8ey7gnhhwg1gqmikwo6 foreign key (userInteractions_id) references Users (id)"; 
+		String alterTableFkQuestion = "alter table Question_Interactions add index FK_plnjd89r1mncrtf1vfj65pspt (Question_id), add constraint FK_plnjd89r1mncrtf1vfj65pspt foreign key (Question_id) references Question (id)";  
 		
 		return RawSQLOperation.forSqls(createTable, alterTableFkUser, alterTableFkQuestion);
 	}
