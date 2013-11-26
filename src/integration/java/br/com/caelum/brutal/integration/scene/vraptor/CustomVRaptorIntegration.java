257985854742f65337b663e1b4c1306b48b2805a
Francisco Sokol <chico.sokol@gmail.com>
2013-11-26 15:56:16 -0200

    primeiro teste de editar questão

--- src/integration/java/br/com/caelum/brutal/integration/scene/vraptor/CustomVRaptorIntegration.java
+++ src/integration/java/br/com/caelum/brutal/integration/scene/vraptor/CustomVRaptorIntegration.java
@@ -2,6 +2,15 @@ package br.com.caelum.brutal.integration.scene.vraptor;
 
 import java.io.IOException;
 
+import org.hibernate.Session;
+
+import br.com.caelum.brutal.dao.InvisibleForUsersRule;
+import br.com.caelum.brutal.dao.QuestionDAO;
+import br.com.caelum.brutal.dao.TagDAO;
+import br.com.caelum.brutal.dao.UserDAO;
+import br.com.caelum.brutal.model.LoggedUser;
+import br.com.caelum.brutal.model.Tag;
+import br.com.caelum.brutal.model.User;
 import br.com.caelum.brutal.util.DataImport;
 import br.com.caelum.vraptor.environment.ServletBasedEnvironment;
 import br.com.caelum.vraptor.test.VRaptorIntegration;
@@ -10,29 +19,59 @@ import br.com.caelum.vraptor.test.requestflow.UserFlow;
 
 public class CustomVRaptorIntegration extends VRaptorIntegration {
 
+	private DataImport dataImport;
+
 	{
-		System.setProperty(ServletBasedEnvironment.ENVIRONMENT_PROPERTY, "acceptance");
-//		importData();
+		System.setProperty(ServletBasedEnvironment.ENVIRONMENT_PROPERTY,
+				"acceptance");
+		dataImport = new DataImport();
+		importData();
+		dataImport.getSession().beginTransaction();
 	}
 
 	private void importData() {
 		try {
-			new DataImport().run();
+			dataImport.run();
 		} catch (IOException e) {
 			throw new RuntimeException(e);
 		}
 	}
 
 	protected UserFlow login(UserFlow navigation, String email, String password) {
-		return navigation.post("/login", Parameters.initWith("email", email).add("password", password));
-		
+		return navigation.post("/login", Parameters.initWith("email", email)
+				.add("password", password));
 	}
 
-	protected UserFlow createQuestion(UserFlow navigation, String title, String description,
-			String tagNames, boolean watching) {
-				return navigation.post("/perguntar", Parameters.initWith("title", title).add("description", description).add("tagNames", tagNames).add("watching", watching));
-			}
+	protected UserFlow createQuestion(UserFlow navigation, String title,
+			String description, String tagNames, boolean watching) {
+		return navigation.post("/perguntar", 
+				Parameters.initWith("title", title)
+					.add("description", description)
+					.add("tagNames", tagNames).add("watching", watching));
+	}
 	
+	protected QuestionDAO questionDao() {
+		InvisibleForUsersRule invisible = new InvisibleForUsersRule(new LoggedUser(null, null));
+		return new QuestionDAO(dataImport.getSession(), invisible);
+	}
 	
+	protected UserDAO userDao() {
+		return new UserDAO(dataImport.getSession());
+	}
+	
+	protected User moderator() {
+		UserDAO users = new UserDAO(dataImport.getSession());
+		return users.findByMailAndPassword("moderator@caelum.com.br", "123456");
+	}
+	
+	protected Tag tag(String name) {
+		return new TagDAO(this.dataImport.getSession()).findByName(name);
+	}
+	
+	protected void commit() {
+		Session session = this.dataImport.getSession();
+		session.getTransaction().commit();
+		session.beginTransaction();
+	}
 	
 }
--- src/integration/java/br/com/caelum/brutal/integration/scene/vraptor/EditAnswerTest.java
+++ src/integration/java/br/com/caelum/brutal/integration/scene/vraptor/EditAnswerTest.java
@@ -1,78 +1,43 @@
 package br.com.caelum.brutal.integration.scene.vraptor;
 
-import static org.junit.Assert.assertTrue;
+import static br.com.caelum.vraptor.test.http.Parameters.initWith;
 
-import java.io.File;
-import java.io.IOException;
+import java.util.Arrays;
+import java.util.List;
 
-import org.apache.commons.io.FileUtils;
-import org.junit.AfterClass;
-import org.junit.Ignore;
+import org.junit.Assert;
 import org.junit.Test;
 
-import br.com.caelum.brutal.integration.pages.QuestionPage;
-import br.com.caelum.vraptor.environment.ServletBasedEnvironment;
+import br.com.caelum.brutal.model.LoggedUser;
+import br.com.caelum.brutal.model.Question;
+import br.com.caelum.brutal.model.QuestionInformation;
 import br.com.caelum.vraptor.test.VRaptorTestResult;
-import br.com.caelum.vraptor.test.http.Parameters;
 import br.com.caelum.vraptor.test.requestflow.UserFlow;
+import br.com.caelum.vraptor.validator.I18nMessage;
 
 public class EditAnswerTest extends CustomVRaptorIntegration {
 
 	@Test
 	public void should_edit_answer_of_other_author() throws Exception {
-		UserFlow navigation = login(navigate(), "moderator@caelum.com.br", "123456");
-		navigation = createQuestion(navigation, "Titulo da questao hahaha", "Descricao da questao longa demais", "java", false);
+		LoggedUser author = new LoggedUser(moderator(), null);
+		QuestionInformation questionInformation = new QuestionInformation("Titulo da questao hahaha", "Descricao da questao longa demais", 
+				author, Arrays.asList(tag("java")), "new question");
+		Question question = new Question(questionInformation, moderator());
+		questionDao().save(question);
+		commit();
 		
-		VRaptorTestResult questionCreated = navigation.followRedirect().execute();
-		questionCreated.wasStatus(200);
-		System.out.println(questionCreated.getLastPath());
+		UserFlow navigation = login(navigate(), "karma.nigga@caelum.com.br", "123456")
+				.post("/pergunta/editar/" + question.getId(), 
+					initWith("title", "new edited title")
+					.add("description", "new edited description new edited description")
+					.add("comment", "edit comment")
+					.add("tagNames", "java"));
+		VRaptorTestResult questionEdited = navigation.followRedirect().execute();
 		
+		questionEdited.wasStatus(200).isValid();
 		
-		
-//    	loginAsModerator();
-//    	home().toNewQuestionPage().newQuestion("question question question question", 
-//    			"description description description description description", "java")
-//    			.answer("answer answer answer answer answer answer answer answer");
-//    	logout();
-//    	
-//        loginWithALotOfKarma();
-//        
-//        QuestionPage questionPage = home().toFirstQuestionPage()
-//            .toEditFirstAnswerPage()
-//            .edit("new answer with more than 30 characters aw  yeah !!!", 
-//                "any comment");
-//        
-//        boolean containsConfirmationMessage = questionPage
-//            .confirmationMessages()
-//            .contains(message("status.pending"));
-//        
-//        assertTrue(containsConfirmationMessage);
-
+		List<I18nMessage> confirmationMessages = questionEdited.getObject("messages");
+		Assert.assertEquals(1, confirmationMessages.size());
 	}
 
-	private UserFlow firstQuestionFromList(UserFlow navigation) {
-		navigation.get("/");
-		return navigation;
-	}
-
-	@Test
-	@Ignore
-	public void simple() throws Exception {
-		VRaptorTestResult result = navigate()
-				.post("/login",
-						Parameters.initWith("email", "moderator@caelum.com.br")
-						.add("password", "123456")
-				).followRedirect().execute();
-		System.out.println("** "+result.getResponseBody());
-//		VRaptorTestResult result = navigate()
-//				.get("/").followRedirect().execute();
-//		System.out.println("** "+result.getResponseBody());
-//		System.out.println(result.getLastPath());
-	}
-	
-	@AfterClass
-	public static void tearDownClass() throws IOException {
-		FileUtils.deleteDirectory(new File("jsp-compilation/"));
-	}
-	
 }
