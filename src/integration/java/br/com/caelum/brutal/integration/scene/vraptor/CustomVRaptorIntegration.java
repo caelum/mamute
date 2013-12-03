141c6155f1879f8237d594d2c694c49dee811c90
Marco Salles <marcosalles@gmail.com>
2013-12-03 16:10:58 -0200

    migracao de EditAnswerTests em andamento dois testes completados com sucesso metodos de suporte genericos criados na classe mae

--- src/integration/java/br/com/caelum/brutal/integration/scene/vraptor/CustomVRaptorIntegration.java
+++ src/integration/java/br/com/caelum/brutal/integration/scene/vraptor/CustomVRaptorIntegration.java
@@ -1,25 +1,38 @@
 package br.com.caelum.brutal.integration.scene.vraptor;
 
+import static br.com.caelum.vraptor.test.http.Parameters.initWith;
+
 import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
 
 import org.hibernate.Session;
 
+import br.com.caelum.brutal.builder.QuestionBuilder;
+import br.com.caelum.brutal.dao.AnswerDAO;
 import br.com.caelum.brutal.dao.InvisibleForUsersRule;
 import br.com.caelum.brutal.dao.QuestionDAO;
 import br.com.caelum.brutal.dao.TagDAO;
 import br.com.caelum.brutal.dao.UserDAO;
+import br.com.caelum.brutal.integration.util.AppMessages;
+import br.com.caelum.brutal.model.Answer;
+import br.com.caelum.brutal.model.AnswerInformation;
 import br.com.caelum.brutal.model.LoggedUser;
+import br.com.caelum.brutal.model.Question;
 import br.com.caelum.brutal.model.Tag;
 import br.com.caelum.brutal.model.User;
 import br.com.caelum.brutal.util.DataImport;
 import br.com.caelum.vraptor.environment.ServletBasedEnvironment;
 import br.com.caelum.vraptor.test.VRaptorIntegration;
-import br.com.caelum.vraptor.test.http.Parameters;
+import br.com.caelum.vraptor.test.VRaptorTestResult;
 import br.com.caelum.vraptor.test.requestflow.UserFlow;
+import br.com.caelum.vraptor.validator.I18nMessage;
 
 public class CustomVRaptorIntegration extends VRaptorIntegration {
 
 	private DataImport dataImport;
+	private AppMessages messages = new AppMessages();
 
 	{
 		System.setProperty(ServletBasedEnvironment.ENVIRONMENT_PROPERTY,
@@ -37,41 +50,118 @@ public class CustomVRaptorIntegration extends VRaptorIntegration {
 		}
 	}
 
-	protected UserFlow login(UserFlow navigation, String email, String password) {
-		return navigation.post("/login", Parameters.initWith("email", email)
-				.add("password", password));
+	protected String message(String key) {
+		return messages.getMessage(key);
 	}
 
-	protected UserFlow createQuestion(UserFlow navigation, String title,
-			String description, String tagNames, boolean watching) {
-		return navigation.post("/perguntar", 
-				Parameters.initWith("title", title)
-					.add("description", description)
-					.add("tagNames", tagNames).add("watching", watching));
+	protected List<String> messagesList(VRaptorTestResult result) {
+		List<I18nMessage> confirmationMessages = result.getObject("messages");
+		List<String> messages = new ArrayList<>();
+		for (I18nMessage message : confirmationMessages) {
+			messages.add(message.getMessage());
+		}
+		return messages;
 	}
-	
+
+	/*** USER FLOW LOGIC ***/
+	protected UserFlow login(UserFlow navigation, String email, String password) {
+		return navigation.post("/login",
+				initWith("email", email).add("password", password));
+	}
+
+	protected UserFlow createQuestionWithFlow(UserFlow navigation,
+			String title, String description, String tagNames, boolean watching) {
+		return navigation.post(
+				"/perguntar",
+				initWith("title", title).add("description", description)
+						.add("tagNames", tagNames).add("watching", watching));
+	}
+
+	protected UserFlow editQuestionWithFlow(UserFlow navigation,
+			Long questionId, String title, String description, String comment,
+			String tags) {
+		return navigation.post(
+				"/pergunta/editar/" + questionId,
+				initWith("title", title).add("description", description)
+						.add("comment", comment).add("tagNames", tags));
+	}
+
+	protected UserFlow answerQuestionWithFlow(UserFlow navigation,
+			Question question, String description, boolean watching) {
+		return navigation.post("/responder/" + question.getId(),
+				initWith("question", question).add("description", description)
+						.add("watching", watching));
+	}
+
+	protected UserFlow editAnswerWithFlow(UserFlow navigation, Answer answer,
+			String description, String comment) {
+		return navigation.post("/resposta/editar/" + answer.getId(),
+				initWith("original", answer).add("description", description)
+						.add("comment", comment));
+	}
+
+	/*** DAO LOGIC ***/
 	protected QuestionDAO questionDao() {
-		InvisibleForUsersRule invisible = new InvisibleForUsersRule(new LoggedUser(null, null));
+		InvisibleForUsersRule invisible = new InvisibleForUsersRule(
+				new LoggedUser(null, null));
 		return new QuestionDAO(dataImport.getSession(), invisible);
 	}
-	
-	protected UserDAO userDao() {
-		return new UserDAO(dataImport.getSession());
+
+	protected AnswerDAO answerDao() {
+		InvisibleForUsersRule invisible = new InvisibleForUsersRule(
+				new LoggedUser(null, null));
+		return new AnswerDAO(dataImport.getSession(), invisible);
 	}
-	
-	protected User moderator() {
-		UserDAO users = new UserDAO(dataImport.getSession());
-		return users.findByMailAndPassword("moderator@caelum.com.br", "123456");
-	}
-	
-	protected Tag tag(String name) {
-		return new TagDAO(this.dataImport.getSession()).findByName(name);
-	}
-	
+
 	protected void commit() {
 		Session session = this.dataImport.getSession();
 		session.getTransaction().commit();
 		session.beginTransaction();
 	}
-	
+
+	protected UserDAO userDao() {
+		return new UserDAO(dataImport.getSession());
+	}
+
+	protected Tag tag(String name) {
+		return new TagDAO(this.dataImport.getSession()).findByName(name);
+	}
+
+	protected User moderator() {
+		UserDAO users = new UserDAO(dataImport.getSession());
+		return users.findByMailAndPassword("moderator@caelum.com.br", "123456");
+	}
+
+	protected User karmaNigga() {
+		UserDAO users = new UserDAO(dataImport.getSession());
+		return users.findByMailAndPassword("karma.nigga@caelum.com.br",
+				"123456");
+	}
+
+	protected User user(String email) {
+		UserDAO users = new UserDAO(dataImport.getSession());
+		return users.findByMailAndPassword(email, "123456");
+	}
+
+	protected Question createQuestionWithDao(User author, String title,
+			String description, Tag... tags) {
+		Question question = new QuestionBuilder().withTitle(title)
+				.withDescription(description).withTags(Arrays.asList(tags))
+				.withAuthor(author).build();
+		questionDao().save(question);
+		commit();
+		return question;
+	}
+
+	protected Answer answerQuestionWithDao(User author, Question question,
+			String description, boolean watching) {
+		LoggedUser loggedUser = new LoggedUser(author, null);
+		AnswerInformation information = new AnswerInformation(description,
+				loggedUser, "new answer");
+		Answer answer = new Answer(information, question, author);
+		answerDao().save(answer);
+		commit();
+		return answer;
+	}
+
 }
--- src/integration/java/br/com/caelum/brutal/integration/scene/vraptor/EditAnswerTest.java
+++ src/integration/java/br/com/caelum/brutal/integration/scene/vraptor/EditAnswerTest.java
@@ -1,43 +1,72 @@
 package br.com.caelum.brutal.integration.scene.vraptor;
 
-import static br.com.caelum.vraptor.test.http.Parameters.initWith;
+import static org.junit.Assert.assertTrue;
 
-import java.util.Arrays;
 import java.util.List;
+import java.util.Set;
 
 import org.junit.Assert;
 import org.junit.Test;
 
-import br.com.caelum.brutal.model.LoggedUser;
+import br.com.caelum.brutal.model.Answer;
+import br.com.caelum.brutal.model.AnswerAndVotes;
 import br.com.caelum.brutal.model.Question;
-import br.com.caelum.brutal.model.QuestionInformation;
 import br.com.caelum.vraptor.test.VRaptorTestResult;
 import br.com.caelum.vraptor.test.requestflow.UserFlow;
-import br.com.caelum.vraptor.validator.I18nMessage;
 
 public class EditAnswerTest extends CustomVRaptorIntegration {
 
 	@Test
 	public void should_edit_answer_of_other_author() throws Exception {
-		LoggedUser author = new LoggedUser(moderator(), null);
-		QuestionInformation questionInformation = new QuestionInformation("Titulo da questao hahaha", "Descricao da questao longa demais", 
-				author, Arrays.asList(tag("java")), "new question");
-		Question question = new Question(questionInformation, moderator());
-		questionDao().save(question);
-		commit();
-		
-		UserFlow navigation = login(navigate(), "karma.nigga@caelum.com.br", "123456")
-				.post("/pergunta/editar/" + question.getId(), 
-					initWith("title", "new edited title")
-					.add("description", "new edited description new edited description")
-					.add("comment", "edit comment")
-					.add("tagNames", "java"));
-		VRaptorTestResult questionEdited = navigation.followRedirect().execute();
-		
-		questionEdited.wasStatus(200).isValid();
-		
-		List<I18nMessage> confirmationMessages = questionEdited.getObject("messages");
-		Assert.assertEquals(1, confirmationMessages.size());
+		Question question = createQuestionWithDao(moderator(),
+				"Titulo da questao hahaha",
+				"Descricao da questao longa demais", tag("java"));
+
+		Answer answer = answerQuestionWithDao(moderator(), question,
+				"Resposta da questao do teste de edicao", false);
+
+		String newDescription = "my new description of the first answer";
+
+		UserFlow navigation = login(navigate(), "karma.nigga@caelum.com.br",
+				"123456");
+		navigation = editAnswerWithFlow(navigation, answer, newDescription,
+				"comment");
+
+		VRaptorTestResult answerEdited = navigation.followRedirect().execute();
+		answerEdited.wasStatus(200).isValid();
+
+		List<String> messagesList = messagesList(answerEdited);
+		assertTrue(messagesList.contains(message("status.pending")));
+	}
+
+	@Test
+	public void should_edit_and_automatically_approve_author_edit()
+			throws Exception {
+		Question question = createQuestionWithDao(moderator(),
+				"Titulo da questao hahaha",
+				"Descricao da questao longa demais", tag("java"));
+
+		Answer answer = answerQuestionWithDao(karmaNigga(), question,
+				"Resposta da questao do teste de edicao", false);
+
+		String newDescription = "my new description of the first answer";
+
+		UserFlow navigation = login(navigate(), "karma.nigga@caelum.com.br",
+				"123456");
+		navigation = editAnswerWithFlow(navigation, answer, newDescription,
+				"comment");
+
+		VRaptorTestResult answerEdited = navigation.followRedirect().execute();
+		answerEdited.wasStatus(200).isValid();
+
+		List<String> messagesList = messagesList(answerEdited);
+		assertTrue(messagesList.contains(message("status.no_need_to_approve")));
+
+		AnswerAndVotes answerAndVotes = answerEdited.getObject("answers");
+		Set<Answer> answers = answerAndVotes.getVotes().keySet();
+		for (Answer answerInSet : answers) {
+			Assert.assertEquals(newDescription, answerInSet.getDescription());
+		}
 	}
 
 }
