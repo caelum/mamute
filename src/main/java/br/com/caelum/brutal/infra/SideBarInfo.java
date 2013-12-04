94a11fadab4a87b245a91a24ab5afdae5a7b047f
leonardowolter <leocwolter@gmail.com>
2013-12-04 12:59:04 -0200

    colocando todas as entradas do FeedsMap na jsp

--- src/main/java/br/com/caelum/brutal/infra/SideBarInfo.java
+++ src/main/java/br/com/caelum/brutal/infra/SideBarInfo.java
@@ -1,10 +1,14 @@
 package br.com.caelum.brutal.infra;
 
+import java.util.Map.Entry;
+import java.util.Set;
+
 import javax.inject.Inject;
 
 import br.com.caelum.brutal.components.RecentTagsContainer;
 import br.com.caelum.brutal.dao.NewsDAO;
 import br.com.caelum.brutal.infra.rss.read.FeedsMap;
+import br.com.caelum.brutal.infra.rss.read.RSSFeed;
 import br.com.caelum.vraptor.Result;
 
 public class SideBarInfo {
@@ -17,6 +21,9 @@ public class SideBarInfo {
 	public void include(){
 		result.include("sidebarNews", newses.allVisibleAndApproved(5));
 		result.include("recentTags", tagsContainer.getRecentTagsUsage());
-		result.include("jobs", feedsMap.get("jobs"));
+		Set<Entry<String, RSSFeed>> entrySet = feedsMap.entrySet();
+		for (Entry<String, RSSFeed> entry : entrySet) {
+			result.include(entry.getKey(), entry.getValue());
+		}
 	}
 }
