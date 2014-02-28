2a344ae342b389743685f3b30c1dffeb083667a9
Francisco Sokol <chico.sokol@gmail.com>
2014-02-28 18:42:25 -0300

    using timestamp in merge assets and calling ant from post-build.sh

--- core/scripts/post-build.sh
+++ core/scripts/post-build.sh
@@ -3,6 +3,10 @@ TIMESTAMP=$1
 
 TARGET_SCRIPT="target/mamute-1.0.0-SNAPSHOT/run.sh"
 
+ant -Dtimestamp=$TIMESTAMP \
+    -lib ant-lib/yuicompressor-2.4.8.jar \
+    -buildfile merge-assets.xml
+
 sed -e "s/BUILD_TIMESTAMP/${TIMESTAMP}/" scripts/run.sh.template \
     > $TARGET_SCRIPT
 
