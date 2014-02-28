f40a7ee96448286240e07a6f5b775dfc903223e5
Francisco Sokol <chico.sokol@gmail.com>
2014-02-28 17:38:52 -0300

    minors in run.sh

--- core/scripts/post-build.sh
+++ core/scripts/post-build.sh
@@ -1,6 +1,11 @@
 
 TIMESTAMP=$1
 
+TARGET_SCRIPT="target/mamute-1.0.0-SNAPSHOT/run.sh"
+
 sed -e "s/BUILD_TIMESTAMP/${TIMESTAMP}/" scripts/run.sh.template \
-    > target/mamute-1.0.0-SNAPSHOT/run.sh
+    > $TARGET_SCRIPT
+
+chmod +x $TARGET_SCRIPT
+
 
