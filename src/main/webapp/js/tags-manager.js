811ac117d7a8ed88b64249024e81347ef39cea4c
Fernanda Bernardo <fernandabernardo94@gmail.com>
2014-03-24 13:38:01 -0300

    changing tags-manager

--- src/main/webapp/js/tags-manager.js
+++ src/main/webapp/js/tags-manager.js
@@ -1,171 +1,173 @@
-var autoCompleteId;
-var TagsManager = function(components){
-	
-	$("*:not(.autocomplete)").click(function(){
-		$(".autocompleted-tags").addClass("hidden");
-	});
+(function() {
 
-	$.getJSON("/pergunta/allTags", function(json){
-		var allTags = json;
+	var autoCompleteId;
+	var TagsManager = function(components){
 
-		var element = $('.autocomplete');
-		for ( var i in components) {
-			var component = components[i];
-			component.forElement(element, allTags);
-		}
-	});
-}
+		$("*:not(.autocomplete)").click(function(){
+			$(".autocompleted-tags").addClass("hidden");
+		});
 
-var AutoCompleteDOM = function(){
-	var splitChar = TAGS_SPLITTER_CHAR;
-	
-	var showAutoCompleteArea = function(target){
-		target.removeClass("hidden");
-		setLoading(target);
-	}
+		$.getJSON("/pergunta/allTags", function(json){
+			var allTags = json;
 
-	var suggestAutoComplete = function(target, tagChunk, input, allTags){
-		if(tagChunk == undefined || tagChunk == splitChar || !tagChunk) return;
-		suggestions = getSuggestions(tagChunk, allTags);
-		showSuggestions(suggestions, target);
-	}
-	
-	function getSuggestions(tagChunk, allTags){
-		var regex = new RegExp(".*"+tagChunk+".*");
-		var suggestions = $(allTags).map(function(index, tag){
-			if(tag.name.match(regex)){
-				return tag;
+			var element = $('.autocomplete');
+			for ( var i in components) {
+				var component = components[i];
+				component.forElement(element, allTags);
 			}
 		});
-			
-		return sortAndTrim(suggestions, tagChunk);
 	}
-	
-	function sortAndTrim(array, tagChunk){
-		return array.sort(function(tagA, tagB){
-			return  tagA.name.indexOf(tagChunk) - tagB.name.indexOf(tagChunk);
-		}).slice(0, 9);
-	}
-	
-	var showSuggestions = function(suggestions, target){
-		if(suggestions.length != 0){
-			var suggestionElements = "";
-			$(suggestions).each(function(index, suggestion){
-				suggestionElements += "<li class='complete-tag'><a class='tag-brutal'>"+suggestion.name+"</a> x "+ suggestion.usageCount;
-				suggestionElements += "<div class='tag-description'>"+suggestion.description+"</div></li>";
+
+	var AutoCompleteDOM = function(){
+		var splitChar = TAGS_SPLITTER_CHAR;
+
+		var showAutoCompleteArea = function(target){
+			target.removeClass("hidden");
+			setLoading(target);
+		}
+
+		var suggestAutoComplete = function(target, tagChunk, input, allTags){
+			if(tagChunk == undefined || tagChunk == splitChar || !tagChunk) return;
+			suggestions = getSuggestions(tagChunk, allTags);
+			showSuggestions(suggestions, target);
+		}
+
+		function getSuggestions(tagChunk, allTags){
+			var regex = new RegExp(".*"+tagChunk+".*");
+			var suggestions = $(allTags).map(function(index, tag){
+				if(tag.name.match(regex)){
+					return tag;
+				}
 			});
-			$(target).html(suggestionElements).removeClass("hidden");
-			
-			$('.autocompleted-tags .complete-tag').click(function(){
-				var self = $(this);
-				insertTagIntoTextArea(self.find(".tag-brutal").text());
-			});
-		}else{
-			target.addClass("hidden");
+
+			return sortAndTrim(suggestions, tagChunk);
+		}
+
+		function sortAndTrim(array, tagChunk){
+			return array.sort(function(tagA, tagB){
+				return  tagA.name.indexOf(tagChunk) - tagB.name.indexOf(tagChunk);
+			}).slice(0, 9);
+		}
+
+		var showSuggestions = function(suggestions, target){
+			if(suggestions.length != 0){
+				var suggestionElements = "";
+				$(suggestions).each(function(index, suggestion){
+					suggestionElements += "<li class='complete-tag'><a class='tag-brutal'>"+suggestion.name+"</a> x "+ suggestion.usageCount;
+					suggestionElements += "<div class='tag-description'>"+suggestion.description+"</div></li>";
+				});
+				$(target).html(suggestionElements).removeClass("hidden");
+
+				$('.autocompleted-tags .complete-tag').click(function(){
+					var self = $(this);
+					insertTagIntoTextArea(self.find(".tag-brutal").text());
+				});
+			}else{
+				target.addClass("hidden");
+			}
+		}
+
+		var insertTagIntoTextArea = function(text) {
+			var input = $('input[name=tagNames]');
+		    var inputValue = input.val();
+		    var vetValue = inputValue.split(splitChar);
+		    vetValue = vetValue.slice(0, vetValue.length - 1); // remove the last tag(that is incomplete)
+		    vetValue.push(text);
+		    input.val(vetValue.join(splitChar)+splitChar);
+		    $(input).valid();
+		    input.focus();
+		    $(".autocompleted-tags").html("");
+		}
+
+		function isNotAControl(key){
+			keyboardCtrlAutoCompleteBox = [13, 27, 37, 38, 39, 40];
+			return $.inArray(key, keyboardCtrlAutoCompleteBox) < 0
+		}
+
+		function escapeSpecialCharacter(tagChunk){
+		   specialCharacters = ['+','.','|','$','*','^','(',')'];
+		   pattern = specialCharacters.join("");
+		   tagChunk = tagChunk.replace(new RegExp("([" + pattern + "])", 'g'), "\\$1");
+		   return tagChunk;
+		}
+
+		return {
+			forElement: function(element, allTags){
+				element.keyup(function(e){
+					var autoCompleteInput = $(this),
+					target = $("#"+autoCompleteInput.data("autocomplete-id")),
+					tagChunk = $(autoCompleteInput.val().split(splitChar)).last().get(0);
+
+					if(!tagChunk){
+						target.addClass("hidden");
+						return;
+					}
+
+
+					if(isNotAControl(e.which)) { 
+						showAutoCompleteArea(target);
+
+						clearTimeout(autoCompleteId);
+
+						autoCompleteId = setTimeout(function(){
+							suggestAutoComplete(target, escapeSpecialCharacter(tagChunk), autoCompleteInput, allTags)
+						},100);
+					}
+
+				});
+			}
 		}
 	}
 
-	var insertTagIntoTextArea = function(text) {
-		var input = $('input[name=tagNames]');
-	    var inputValue = input.val();
-	    var vetValue = inputValue.split(splitChar);
-	    vetValue = vetValue.slice(0, vetValue.length - 1); // remove the last tag(that is incomplete)
-	    vetValue.push(text);
-	    input.val(vetValue.join(splitChar)+splitChar);
-	    $(input).valid();
-	    input.focus();
-	    $(".autocompleted-tags").html("");
-	}
-	
-	function isNotAControl(key){
-		keyboardCtrlAutoCompleteBox = [13, 27, 37, 38, 39, 40];
-		return $.inArray(key, keyboardCtrlAutoCompleteBox) < 0
-	}
-	
-	function escapeSpecialCharacter(tagChunk){
-	   specialCharacters = ['+','.','|','$','*','^','(',')'];
-	   pattern = specialCharacters.join("");
-	   tagChunk = tagChunk.replace(new RegExp("([" + pattern + "])", 'g'), "\\$1");
-	   return tagChunk;
-	}
-	
-	return {
-		forElement: function(element, allTags){
-			element.keyup(function(e){
-				var autoCompleteInput = $(this),
-				target = $("#"+autoCompleteInput.data("autocomplete-id")),
-				tagChunk = $(autoCompleteInput.val().split(splitChar)).last().get(0);
-				
-				if(!tagChunk){
-					target.addClass("hidden");
-					return;
-				}
-				
-				
-				if(isNotAControl(e.which)) { 
-					showAutoCompleteArea(target);
-					
-					clearTimeout(autoCompleteId);
-					
-					autoCompleteId = setTimeout(function(){
-						suggestAutoComplete(target, escapeSpecialCharacter(tagChunk), autoCompleteInput, allTags)
-					},100);
-				}
+	var TagsNavigation = function(){
+		return {
+			forElement: function(element){
+				pos = -3;
+				element.keydown(function(e){
+					arrow = {left: 37, up: 38, right: 39, down: 40};
+					control = {esc: 27, enter: 13};
+					var completeTag = $('.complete-tag');
 
-			});
+					switch(e.which) {
+						case arrow.down:
+							if(pos < completeTag.length - 3) pos += 3;
+							break;
+
+						case arrow.up:
+							if(pos > 0) pos -= 3;
+							break;
+
+						case arrow.right:
+							if(pos < completeTag.length - 1) pos++;
+							break;
+
+						case arrow.left:
+							if(pos > 0)	pos--;
+							break;
+
+						case control.enter:
+							e.preventDefault();
+							completeTag.eq(pos).click();
+							pos=-3;
+							$('.autocompleted-tags').addClass('hidden');
+
+						case control.esc:
+							$('.autocompleted-tags').addClass('hidden');
+							break;
+
+						default: return;
+					}
+
+					completeTag.removeClass('tag-selected');
+					completeTag.eq(pos).addClass('tag-selected');
+				});		
+			}
+
 		}
+
 	}
-}
 
-var TagsNavigation = function(){
-	return {
-		forElement: function(element){
-			pos = -3;
-			element.keydown(function(e){
-				arrow = {left: 37, up: 38, right: 39, down: 40};
-				control = {esc: 27, enter: 13};
-				var completeTag = $('.complete-tag');
-
-				switch(e.which) {
-					case arrow.down:
-						if(pos < completeTag.length - 3) pos += 3;
-						break;
-						
-					case arrow.up:
-						if(pos > 0) pos -= 3;
-						break;
-						
-					case arrow.right:
-						if(pos < completeTag.length - 1) pos++;
-						break;
-						
-					case arrow.left:
-						if(pos > 0)	pos--;
-						break;
-						
-					case control.enter:
-						e.preventDefault();
-						completeTag.eq(pos).click();
-						pos=-3;
-						$('.autocompleted-tags').addClass('hidden');
-						
-					case control.esc:
-						$('.autocompleted-tags').addClass('hidden');
-						break;
-						
-					default: return;
-				}
-
-				completeTag.removeClass('tag-selected');
-				completeTag.eq(pos).addClass('tag-selected');
-			});		
-		}
-		
-	}
-	
-}
-
-var TagsValidator = function(){
+	var TagsValidator = function(){
 	var splitChar = TAGS_SPLITTER_CHAR;
 	return {
 		forElement : function(element, allTags){
@@ -174,7 +176,7 @@ var TagsValidator = function(){
 					return element.name;
 				})
 			);
-			
+
 			var tagsNotFound = [];
 			$.validator.addMethod(
 					"only-existent-tags",
@@ -188,7 +190,7 @@ var TagsValidator = function(){
 						return "Use apenas tags que existem! As seguintes tags não existem: <b>"+ tagsNotFound + "</b>";
 					}
 			);
-			
+
 			function verifyIfExists(tags, tagsNotFound){
 				tagsNotFound = [];
 				tags.each(function(index, tag){
@@ -198,24 +200,21 @@ var TagsValidator = function(){
 			});
 				return tagsNotFound;
 			}
-			
+
 			function notContains(array, item){
 				return array.indexOf(item) < 0;
 			}
-			
+
 		}
-		
+
 	}
-		
+
 }
 
-(function() {
 	var components = [new AutoCompleteDOM(), new TagsNavigation()];
 	if (ANYONE_CAN_CREATE_TAGS != "true") {
 		components.push(new TagsValidator());
 	}
 	new TagsManager(components);
-	
+
 }());
-
-
