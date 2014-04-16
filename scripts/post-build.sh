1b22a6f9eaa4df9238601260afcc5a43efe2b503
Fernanda Bernardo <fernandabernardo94@gmail.com>
2014-04-16 10:56:53 -0300

    fixing update script

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -1,15 +1,20 @@
 
 TIMESTAMP=$1
 
-TARGET_SCRIPT="target/mamute-1.0.0-SNAPSHOT/run.sh"
+TARGET_SCRIPT_RUN="target/mamute-1.0.0-SNAPSHOT/run.sh"
+TARGET_SCRIPT_UPDATE="target/mamute-1.0.0-SNAPSHOT/update-mamute.sh"
 
 ant -Dtimestamp=$TIMESTAMP \
     -lib ant-lib/yuicompressor-2.4.8.jar \
     -buildfile merge-assets.xml
 
 sed -e "s/BUILD_TIMESTAMP/${TIMESTAMP}/" scripts/run.sh.template \
-    > $TARGET_SCRIPT
+    > $TARGET_SCRIPT_RUN
 
-chmod +x $TARGET_SCRIPT
+sed -e "s/BUILD_TIMESTAMP/${TIMESTAMP}/" scripts/update-mamute.sh.template \
+    > $TARGET_SCRIPT_UPDATE
+
+chmod +x $TARGET_SCRIPT_RUN
+chmod +x $TARGET_SCRIPT_UPDATE
 
 
