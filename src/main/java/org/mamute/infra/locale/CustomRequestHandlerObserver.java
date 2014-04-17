456693ba82b4bc7c97b06712d80fd59d1e85064b
Francisco Sokol <chico.sokol@gmail.com>
2014-04-17 18:31:55 -0300

    using environment define locale

---
+++ src/main/java/org/mamute/infra/locale/CustomRequestHandlerObserver.java
@@ -0,0 +1,45 @@
+package org.mamute.infra.locale;
+
+import javax.enterprise.event.Event;
+import javax.enterprise.event.Observes;
+import javax.enterprise.inject.Specializes;
+import javax.inject.Inject;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.jsp.jstl.core.Config;
+
+import br.com.caelum.vraptor.controller.ControllerNotFoundHandler;
+import br.com.caelum.vraptor.controller.MethodNotAllowedHandler;
+import br.com.caelum.vraptor.core.InterceptorStack;
+import br.com.caelum.vraptor.environment.Environment;
+import br.com.caelum.vraptor.events.ControllerFound;
+import br.com.caelum.vraptor.events.RequestSucceded;
+import br.com.caelum.vraptor.events.VRaptorRequestStarted;
+import br.com.caelum.vraptor.http.UrlToControllerTranslator;
+import br.com.caelum.vraptor.observer.RequestHandlerObserver;
+
+@Specializes
+public class CustomRequestHandlerObserver extends RequestHandlerObserver {
+
+	@Inject
+	private Environment environment;
+
+	@Inject
+	public CustomRequestHandlerObserver(UrlToControllerTranslator translator,
+			ControllerNotFoundHandler controllerNotFoundHandler,
+			MethodNotAllowedHandler methodNotAllowedHandler,
+			Event<ControllerFound> controllerFoundEvent,
+			Event<RequestSucceded> endRequestEvent,
+			InterceptorStack interceptorStack) {
+		super(translator, controllerNotFoundHandler, methodNotAllowedHandler,
+				controllerFoundEvent, endRequestEvent, interceptorStack);
+	}
+	
+	@Override
+	public void handle(@Observes VRaptorRequestStarted requestStarted) {
+		String locale = environment.get("locale", "en");
+		
+		HttpServletRequest request = requestStarted.getRequest();
+		request.setAttribute("javax.servlet.jsp.jstl.fmt.fallbackLocale.request", locale);
+		super.handle(requestStarted);
+	}
+}
