6dde03bd502e967cae5301485c11d7bbf766f613
Francisco Sokol <chico.sokol@gmail.com>
2014-04-22 19:20:03 -0300

    improving integration with js

--- src/main/java/org/mamute/model/MarkDown.java
+++ src/main/java/org/mamute/model/MarkDown.java
@@ -2,24 +2,35 @@ package org.mamute.model;
 
 import java.io.InputStreamReader;
 
-import javax.script.Bindings;
-import javax.script.ScriptContext;
+import javax.script.Invocable;
 import javax.script.ScriptEngine;
 import javax.script.ScriptEngineManager;
 import javax.script.ScriptException;
 
 public class MarkDown {
+	
+	private static ScriptEngine js;
+
+	static {
+		loadJs();
+	}
+
+	private static void loadJs() {
+		MarkDown.js = new ScriptEngineManager().getEngineByName("javascript");
+		try {
+			js.eval(new InputStreamReader(MarkDown.class.getResourceAsStream("/marked.js")));
+		} catch (ScriptException e) {
+			throw new RuntimeException(e);
+		}
+	}
 
 	public synchronized static String parse(String content) {
 		if(content == null || content.isEmpty()) return null;
-		ScriptEngine js = new ScriptEngineManager().getEngineByName("javascript");
-		Bindings bindings = js.getBindings(ScriptContext.ENGINE_SCOPE);
 		try {
-			js.eval(new InputStreamReader(MarkDown.class.getResourceAsStream("/marked.js")));
-			bindings.put("content", content);
-			Object result = js.eval("marked(content)");
+			Invocable invocable = (Invocable) js;
+			Object result = invocable.invokeFunction("marked", content);
 			return result.toString();
-		} catch (ScriptException e) {
+		} catch (ScriptException | NoSuchMethodException e) {
 			throw new RuntimeException(e);
 		}
 	}
