846b655e96b6d20b46db616014b8c6996b2b23dd
Yuri Padilha <yuri.m.padilha@gmail.com>
2014-04-23 19:50:59 -0300

    sample image now looks like a sample image non-interactable

---
+++ javascript/script.js
@@ -0,0 +1,12 @@
+// function fadeImage() {
+// 	var element = document.getElementsByClassName('screenshot')[0];
+// 	element.style.opacity = "0.5";
+// 	element.style.filter  = 'alpha(opacity=90)'; // IE fallback
+// 	element.style.textShadow = '0px 0px 1px black';
+// }
+
+// function unfadeImage() {
+// 	var element = document.getElementsByClassName('screenshot')[0];
+// 	element.style.opacity = "1";
+// 	element.style.filter  = 'alpha(opacity=90)'; // IE fallback
+// }
