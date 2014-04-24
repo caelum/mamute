f3f2aaf75278d21da64907ded2b8b720c1da7031
Francisco Sokol <chico.sokol@gmail.com>
2014-04-24 19:15:26 -0300

    fixing versioning issues

--- scripts/post-build.sh
+++ scripts/post-build.sh
@@ -1,8 +1,10 @@
 
 TIMESTAMP=$1
 
-TARGET_SCRIPT_RUN="target/mamute-1.0.0-SNAPSHOT/run.sh"
-TARGET_SCRIPT_UPDATE="target/mamute-1.0.0-SNAPSHOT/update-mamute.sh"
+VERSION=1.0.1-SNAPSHOT
+
+TARGET_SCRIPT_RUN="target/mamute-$VERSION/run.sh"
+TARGET_SCRIPT_UPDATE="target/mamute-$VERSION/update-mamute.sh"
 
 ant -Dtimestamp=$TIMESTAMP \
     -lib ant-lib/yuicompressor-2.4.8.jar \
