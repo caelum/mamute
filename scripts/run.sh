5bd05811545c6fa448428e9b6e407213ec2a2fc0
Matheus Messora <tqi_mmessora@uolinc.com>
2014-05-08 19:22:05 -0300

    local commit

---
+++ scripts/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+MAMUTE_DIR=$SCRIPT_DIR
+TIMESTAMP=20140507201203
+WEB_INF=${SCRIPT_DIR}/WEB-INF
+
+java -cp ${WEB_INF}/classes/:${WEB_INF}/lib/* \
+    -Ddeploy.timestamp=$TIMESTAMP \
+    -Dvraptor.webappdir=${MAMUTE_DIR} \
+    ${MAMUTE_OPTS} \
+    br.com.caelum.vraptor.server.Main
