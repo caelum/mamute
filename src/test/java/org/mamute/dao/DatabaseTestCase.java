eff35f38ab64d7af329d6289cbf2aa9e97ababdd
Matheus Messora <matheus.messora.vpn@gmail.com>
2014-05-09 02:16:17 -0300

    Changed TestClass to inject the SessionFactory provided by vraptor-hibernate

--- src/test/java/org/mamute/dao/DatabaseTestCase.java
+++ src/test/java/org/mamute/dao/DatabaseTestCase.java
@@ -5,13 +5,13 @@ import br.com.caelum.vraptor.environment.Environment;
 import br.com.caelum.vraptor.ioc.cdi.CDIBasedContainer;
 import br.com.caelum.vraptor.test.container.CdiContainer;
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
 import org.junit.After;
 import org.junit.Before;
 import org.mamute.model.User;
 import org.mamute.model.interfaces.Identifiable;
 
 import javax.enterprise.inject.spi.CDI;
-import javax.inject.Inject;
 import javax.validation.ValidatorFactory;
 import java.io.IOException;
 
@@ -21,45 +21,50 @@ import static br.com.caelum.vraptor.environment.ServletBasedEnvironment.ENVIRONM
 @SuppressWarnings("unchecked")
 public abstract class DatabaseTestCase extends TestCase {
 
-    @Inject
-	protected Session session;
+    protected Session session;
+    protected User loggedUser;
 
-	protected User loggedUser;
+    private static final SessionFactory factory;
 
-	static {
-		try {
-			System.setProperty(ENVIRONMENT_PROPERTY, "test");
-			CdiContainer cdiContainer = new CdiContainer();
-			cdiContainer.start();
-			Environment testing = new DefaultEnvironment(TEST);
-			CDIBasedContainer cdiBasedContainer = CDI.current().select(CDIBasedContainer.class).get();
-			ValidatorFactory vf = cdiBasedContainer.instanceFor(ValidatorFactory.class);
-		} catch (IOException e) {
-			throw new RuntimeException(e);
-		}
-	}
-	
-	protected <T> T save(T obj) {
-		session.save(obj);
-		return obj;
-	}
+    static {
+        try {
+            System.setProperty(ENVIRONMENT_PROPERTY, "test");
+            CdiContainer cdiContainer = new CdiContainer();
+            cdiContainer.start();
+            Environment testing = new DefaultEnvironment(TEST);
+            CDIBasedContainer cdiBasedContainer = CDI.current().select(CDIBasedContainer.class).get();
+            ValidatorFactory vf = cdiBasedContainer.instanceFor(ValidatorFactory.class);
+            factory =  cdiBasedContainer.instanceFor(SessionFactory.class);
+//            creator = new SessionFactoryCreator(testing, vf);
+//            creator.init();
+//            factory = creator.getInstance();
+        } catch (IOException e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-	@Before
-	public void beforeDatabase() {
-		session.beginTransaction();
-	}
+    protected <T> T save(T obj) {
+        session.save(obj);
+        return obj;
+    }
 
-	@After
-	public void afterDatabase() {
-		boolean wasActive = session.getTransaction().isActive();
-		if (wasActive) {
-			session.getTransaction().rollback();
-		}
-		session.close();
-	}
-	
-	public <T extends Identifiable> T reload(T obj) {
-		session.evict(obj);
-		return (T) session.load(obj.getClass(), obj.getId());
-	}
+    @Before
+    public void beforeDatabase() {
+        session = factory.openSession();
+        session.beginTransaction();
+    }
+
+    @After
+    public void afterDatabase() {
+        boolean wasActive = session.getTransaction().isActive();
+        if (wasActive) {
+            session.getTransaction().rollback();
+        }
+        session.close();
+    }
+
+    public <T extends Identifiable> T reload(T obj) {
+        session.evict(obj);
+        return (T) session.load(obj.getClass(), obj.getId());
+    }
 }
