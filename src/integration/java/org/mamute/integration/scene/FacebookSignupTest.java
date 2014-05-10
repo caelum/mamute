e8c2d83b0616442087e92cc361b5b80059017e45
Matheus Messora <matheus.messora.vpn@gmail.com>
2014-05-10 18:48:06 -0300

    Better log information for FB Test

--- src/integration/java/org/mamute/integration/scene/FacebookSignupTest.java
+++ src/integration/java/org/mamute/integration/scene/FacebookSignupTest.java
@@ -1,12 +1,6 @@
 package org.mamute.integration.scene;
 
-import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertTrue;
-import static org.junit.Assert.fail;
-
-import java.io.IOException;
-import java.net.URLEncoder;
-
+import com.google.gson.JsonParser;
 import org.apache.commons.httpclient.HttpException;
 import org.apache.commons.httpclient.URIException;
 import org.apache.commons.httpclient.methods.GetMethod;
@@ -14,29 +8,44 @@ import org.junit.Test;
 import org.mamute.auth.OAuthServiceCreator;
 import org.mamute.integration.pages.FacebookLoginPage;
 import org.mamute.integration.pages.Home;
+import org.slf4j.Logger;
 
-import com.google.gson.JsonParser;
+import java.io.IOException;
+import java.net.URLEncoder;
+import java.util.NoSuchElementException;
+
+import static org.junit.Assert.*;
+import static org.slf4j.LoggerFactory.getLogger;
 
 public class FacebookSignupTest extends AcceptanceTestBase {
+
+    private static final Logger LOG = getLogger(FacebookSignupTest.class);
+
 	
 	@Test
 	public void should_signup_and_login_through_facebook() throws Exception {
-		String appToken = getAppToken();
-		FacebookUser facebookUser = createFacebookTestUser(appToken);
-		FacebookLoginPage signupWithFacebook = home().toLoginPage().signupWithFacebook();
-		Home home = signupWithFacebook
-			.writeEmail(facebookUser.email)
-			.writePassword(facebookUser.password)
-			.submit().confirm();
-		
-		assertTrue(home.isLoggedIn());
-		assertEquals(facebookUser.email, home.toProfilePage().userEmail());
-		
-		home().logOut();
-		
-		boolean loggedInThroughFacebook = home().toLoginPage().loginThroughFacebook().isLoggedIn();
-		assertTrue(loggedInThroughFacebook);
-	}
+        String appToken;
+        try {
+            appToken = getAppToken();
+        }catch (NoSuchElementException exc) {
+            LOG.warn("Did you remember to put your facebook APP_ID and Secret in mamute.properties?", exc.getMessage());
+            throw exc;
+        }
+        FacebookUser facebookUser = createFacebookTestUser(appToken);
+        FacebookLoginPage signupWithFacebook = home().toLoginPage().signupWithFacebook();
+        Home home = signupWithFacebook
+                .writeEmail(facebookUser.email)
+                .writePassword(facebookUser.password)
+                .submit().confirm();
+
+        assertTrue(home.isLoggedIn());
+        assertEquals(facebookUser.email, home.toProfilePage().userEmail());
+
+        home().logOut();
+
+        boolean loggedInThroughFacebook = home().toLoginPage().loginThroughFacebook().isLoggedIn();
+        assertTrue(loggedInThroughFacebook);
+    }
 	
 	@SuppressWarnings("deprecation")
 	private FacebookUser createFacebookTestUser(String appToken) throws URIException,
