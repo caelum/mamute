24612d61d3e1a7dea6e4c6c15cc2cda7a1d33d45
Paulo Jr <paulojribp@gmail.com>
2014-05-27 16:39:20 -0300

    Separating logic from the controller as requested by @csokol

--- src/main/java/org/mamute/controllers/MessagesController.java
+++ src/main/java/org/mamute/controllers/MessagesController.java
@@ -1,12 +1,10 @@
 package org.mamute.controllers;
 
-import java.io.IOException;
-import java.util.HashMap;
 import java.util.Map;
-import java.util.Properties;
-import java.util.Set;
 
 import javax.inject.Inject;
+
+import org.mamute.infra.locale.MessagesLoader;
 
 import br.com.caelum.vraptor.Controller;
 import br.com.caelum.vraptor.Get;
@@ -18,38 +16,18 @@ import br.com.caelum.vraptor.view.Results;
 public class MessagesController {
 
 	@Inject
+	private MessagesLoader loader;
+
+	@Inject
 	private Environment env;
 
-	@Inject private Result result;
+	@Inject
+	private Result result;
 
 	@Get("/messages/loadAll")
 	public void loadMessages() {
 		String locale = env.get("locale", "en");
-
-		Properties props = new Properties();
-		Map<String, String> messages = new HashMap<String, String>();
-		try {
-			props.load(this.getClass().getResourceAsStream("/messages.properties"));
-			Set<Object> keySet = props.keySet();
-			for (Object key : keySet) {
-				String value = (String) props.get(key);
-				messages.put((String) key, value);
-			}
-
-			if (!locale.equals("en")) {
-				props.load(this.getClass().getResourceAsStream("/messages_"+locale+".properties"));
-				keySet = props.keySet();
-				for (Object key : keySet) {
-					String value = (String) props.get(key);
-					messages.put((String) key, value);
-				}
-			}
-
-		} catch (IOException e) {
-			result.nothing();
-			return;
-		}
-
+		Map<String, String> messages = loader.loadBy(locale);
 		result.use(Results.json()).from(messages).serialize();
 	}
 
