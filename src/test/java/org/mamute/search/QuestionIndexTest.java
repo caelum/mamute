202bd13a8ed8ba9d8f71671b5bd9141db3ec4ef3
Taylor Jones <monitorjbl@gmail.com>
2014-08-15 14:41:07 -0400

    adding first solr integration as part of issue #9

---
+++ src/test/java/org/mamute/search/QuestionIndexTest.java
@@ -0,0 +1,127 @@
+package org.mamute.search;
+
+import com.google.common.io.Resources;
+import org.apache.solr.client.solrj.SolrServerException;
+import org.junit.*;
+import org.mamute.builder.QuestionBuilder;
+import org.mamute.dao.InvisibleForUsersRule;
+import org.mamute.dao.QuestionDAO;
+import org.mamute.model.*;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+public class QuestionIndexTest extends SolrTestCase {
+	private static final Logger LOGGER = LoggerFactory.getLogger(QuestionIndexTest.class);
+	static QuestionIndex sut;
+	static QuestionDAO dao;
+
+	static User author;
+	static QuestionBuilder question = new QuestionBuilder();
+
+	static Tag eli5;
+	static Tag eli12;
+	static Tag science;
+	static Tag hobby;
+	static Tag tolstoy;
+
+	@Test
+	public void testFindQuestionsByTitle() {
+		List<Question> questions = sut.findQuestionsByTitle("sky blue", 3);
+		assertEquals(1, questions.size());
+		assertEquals("Why is the sky blue?", questions.get(0).getTitle());
+	}
+
+	@Test
+	public void testFindQuestionsByTitleAndTag() {
+		List<Question> questions = sut.findQuestionsByTitleAndTag("Where", Arrays.asList(eli12), 3);
+		assertEquals(3, questions.size());
+		assertEquals("Where do babies come from?", questions.get(0).getTitle());
+	}
+
+	@BeforeClass
+	public static void setup() throws IOException, SolrServerException {
+		author = new User("Leonardo", "leo@leo");
+		author.confirmEmail();
+		hibernateSession.save(author);
+
+		eli5 = new Tag("eli5", "Explain Like I'm 5", author);
+		hibernateSession.save(eli5);
+		eli12 = new Tag("eli12", "Explain Like I'm 12", author);
+		hibernateSession.save(eli12);
+		science = new Tag("science", "Science!", author);
+		hibernateSession.save(science);
+		hobby = new Tag("hobby", "Hobbies and stuff", author);
+		hibernateSession.save(hobby);
+		tolstoy = new Tag("tolstoy", "Long-winded russian author", author);
+		hibernateSession.save(tolstoy);
+
+		dao = new QuestionDAO(hibernateSession, new InvisibleForUsersRule(new LoggedUser(author, null)));
+		sut = new QuestionIndex(solrServer, dao);
+		data();
+	}
+
+	@AfterClass
+	public static void cleanup() throws IOException, SolrServerException {
+		solrServer.deleteByQuery("*:*");
+	}
+
+	static void data() throws IOException, SolrServerException {
+		solrServer.deleteByQuery("*:*");
+
+		Question q;
+		try (BufferedReader fis = new BufferedReader(new InputStreamReader(Resources.getResource("index/war-peace.txt").openStream()))) {
+			String line;
+			long count = 4;
+			List<Question> questions = new ArrayList<>();
+
+			//load first 500 lines of war and peace
+			LOGGER.info("Warring for peace");
+			while ((line = fis.readLine()) != null) {
+				if (line.length() > QuestionInformation.TITLE_MIN_LENGTH &&
+						line.length() < QuestionInformation.TITLE_MAX_LENGTH) {
+					q = question
+							.withTitle(line)
+							.withTags(Arrays.asList(tolstoy))
+							.withAuthor(author)
+							.build();
+					dao.save(q);
+					questions.add(q);
+				}
+			}
+			LOGGER.info("Peace achieved");
+
+			q = question
+					.withTitle("Why is the sky blue?")
+					.withTags(Arrays.asList(science, eli5))
+					.withAuthor(author)
+					.build();
+			dao.save(q);
+			sut.indexQuestion(q);
+
+			q = question
+					.withTitle("Where do babies come from?")
+					.withTags(Arrays.asList(science, eli12))
+					.withAuthor(author)
+					.build();
+			dao.save(q);
+			sut.indexQuestion(q);
+
+			q = question
+					.withTitle("How do they get the ship in the bottle?")
+					.withTags(Arrays.asList(hobby))
+					.withAuthor(author)
+					.build();
+			dao.save(q);
+			sut.indexQuestion(q);
+
+			sut.indexQuestionBatch(questions);
+		}
+	}
+}
