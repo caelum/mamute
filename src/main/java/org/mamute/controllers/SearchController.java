5d85144dc40c30c990ee881197e66e7c633876ba
Taylor Jones <monitorjbl@gmail.com>
2014-08-19 00:05:08 -0400

    adding indexing to question dao layer (#9)
    adding basic search view at /indexSearch

--- src/main/java/org/mamute/controllers/SearchController.java
+++ src/main/java/org/mamute/controllers/SearchController.java
@@ -2,6 +2,8 @@ package org.mamute.controllers;
 
 import javax.inject.Inject;
 
+import org.mamute.dao.QuestionDAO;
+import org.mamute.model.Question;
 import org.mamute.sanitizer.HtmlSanitizer;
 
 import br.com.caelum.vraptor.Controller;
@@ -9,18 +11,54 @@ import br.com.caelum.vraptor.Get;
 import br.com.caelum.vraptor.Result;
 import br.com.caelum.vraptor.environment.Environment;
 import br.com.caelum.vraptor.routes.annotation.Routed;
+import org.mamute.search.QuestionIndex;
+
+import java.util.ArrayList;
+import java.util.List;
 
 @Routed
 @Controller
 public class SearchController {
-    
-    @Inject private Result result;
-	@Inject private Environment env;
-    
-    @Get
-    public void search(String query) {
-    	String sanitized = HtmlSanitizer.sanitize(query);
-    	result.include("customGoogleSearchKey", env.get("custom_google_search_key"));
-        result.include("query", sanitized);
-    }
+
+	@Inject
+	private Result result;
+	@Inject
+	private Environment env;
+	@Inject
+	private QuestionIndex index;
+	@Inject
+	private QuestionDAO dao;
+
+	@Get
+	public void search(String query) {
+		String sanitized = HtmlSanitizer.sanitize(query);
+		result.include("customGoogleSearchKey", env.get("custom_google_search_key"));
+		result.include("query", sanitized);
+	}
+
+	@Get
+	public void indexSearch(String query) {
+		String sanitized = HtmlSanitizer.sanitize(query);
+		List<Long> ids = index.findQuestionsByTitle(sanitized, 10);
+		List<Question> questions = new ArrayList<>();
+		for (Long id : ids) {
+			questions.add(dao.getById(id));
+		}
+
+		result.include("results", questions);
+		result.include("query", sanitized);
+	}
+
+	@Get
+	public void indexSync() {
+		long pages = dao.numberOfPages();
+		long total = 0;
+		for (int i = 0; i < pages; i++) {
+			List<Question> q = dao.allVisible(i);
+			index.indexQuestionBatch(q);
+			total += q.size();
+		}
+
+		result.include("total", total);
+	}
 }
