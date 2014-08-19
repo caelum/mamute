57387239d66220a54776f054472a039e046ef1f6
Taylor Jones <monitorjbl@gmail.com>
2014-08-18 22:00:57 -0400

    adding embedded solr option (#9)
    adding basic embedded solr test home dir
    
    cleaning up loggingoutput for embedded solr
    
    making SolrTestCase abstract

--- src/main/java/org/mamute/providers/SolrServerProvider.java
+++ src/main/java/org/mamute/providers/SolrServerProvider.java
@@ -1,8 +1,10 @@
 package org.mamute.providers;
 
+import br.com.caelum.vraptor.environment.Environment;
 import org.apache.solr.client.solrj.SolrServer;
+import org.apache.solr.client.solrj.embedded.EmbeddedSolrServer;
 import org.apache.solr.client.solrj.impl.HttpSolrServer;
-import org.mamute.vraptor.environment.MamuteEnvironment;
+import org.apache.solr.core.CoreContainer;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
@@ -15,6 +17,10 @@ import javax.enterprise.inject.Produces;
 import javax.inject.Inject;
 import javax.interceptor.Interceptor;
 
+import java.io.File;
+
+import static org.apache.commons.lang.StringUtils.isNotEmpty;
+
 @Alternative
 @Priority(Interceptor.Priority.APPLICATION)
 public class SolrServerProvider {
@@ -22,18 +28,33 @@ public class SolrServerProvider {
 			.getLogger(SolrServerProvider.class);
 
 	SolrServer server;
-	MamuteEnvironment env;
+	Environment env;
 
 	@Inject
-	public SolrServerProvider(MamuteEnvironment env) {
+	public SolrServerProvider(Environment env) {
 		this.env = env;
 	}
 
 	@PostConstruct
 	public void create() {
-		LOGGER.info("Connecting to localhost");
-		if (env.has("solr.url")) {
-			server = new HttpSolrServer(env.get("solr.url"));
+		//embedded options
+		Boolean embedded = Boolean.parseBoolean(env.get("solr.embedded", "false"));
+		String solrHome = env.get("solr.home", "");
+		String solrCore = env.get("solr.core", "");
+
+		//remote options
+		String remoteUrl = env.get("solr.url", "");
+
+		if (embedded && isNotEmpty(solrHome)) {
+			LOGGER.info("Starting embedded Solr");
+			CoreContainer coreContainer = new CoreContainer(new File(solrHome).getAbsolutePath());
+			coreContainer.load();
+			server = new EmbeddedSolrServer(coreContainer, solrCore);
+		} else if (isNotEmpty(remoteUrl)) {
+			LOGGER.info("Connecting to external Solr at [" + remoteUrl + "]");
+			server = new HttpSolrServer(remoteUrl);
+		} else {
+			LOGGER.warn("Solr config options not valid, not initting");
 		}
 	}
 
--- src/test/java/org/mamute/search/SolrTestCase.java
+++ src/test/java/org/mamute/search/SolrTestCase.java
@@ -1,20 +1,37 @@
 package org.mamute.search;
 
+import com.google.common.io.Resources;
+import org.apache.commons.io.FileUtils;
 import org.apache.solr.client.solrj.SolrServer;
 import org.hibernate.Session;
 import org.junit.BeforeClass;
+import org.junit.Ignore;
 import org.mamute.dao.DatabaseTestCase;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-public class SolrTestCase extends DatabaseTestCase {
+import java.io.File;
+import java.io.IOException;
+
+public abstract class SolrTestCase extends DatabaseTestCase {
+	private static final Logger LOGGER = LoggerFactory.getLogger(SolrTestCase.class);
+
 	static SolrServer solrServer;
 	static Session hibernateSession;
 
 	static {
-		solrServer = cdiBasedContainer.instanceFor(SolrServer.class);
+		//copy Solr home to target
+		try {
+			FileUtils.copyDirectory(new File(Resources.getResource("solr").getPath()), new File("target/solr"));
+			solrServer = cdiBasedContainer.instanceFor(SolrServer.class);
+		} catch (Exception e) {
+			LOGGER.error(e.getMessage(), e);
+			throw new RuntimeException(e);
+		}
 	}
 
 	@BeforeClass
-	public static void openSession(){
+	public static void openSession() {
 		hibernateSession = factory.openSession();
 		hibernateSession.beginTransaction();
 	}
