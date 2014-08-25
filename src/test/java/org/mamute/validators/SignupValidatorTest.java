64aeefdcf7c4942d89ce5c5d302daad0591e80fe
leonardowolter <leocwolter@gmail.com>
2014-08-25 18:27:27 -0300

    deleting unused class, not allowing existent user names and tests. closes #46

--- src/test/java/org/mamute/validators/SignupValidatorTest.java
+++ src/test/java/org/mamute/validators/SignupValidatorTest.java
@@ -2,7 +2,6 @@ package org.mamute.validators;
 
 import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
-import static org.mockito.Mockito.mock;
 import static org.mockito.Mockito.when;
 
 import java.util.ResourceBundle;
@@ -11,61 +10,65 @@ import javax.validation.Validation;
 
 import org.junit.Before;
 import org.junit.Test;
+import org.junit.runner.RunWith;
 import org.mamute.controllers.BrutalValidator;
 import org.mamute.dao.TestCase;
 import org.mamute.dao.UserDAO;
 import org.mamute.factory.MessageFactory;
 import org.mamute.model.User;
-import org.mamute.validators.EmailValidator;
-import org.mamute.validators.SignupValidator;
-import org.mamute.validators.UserValidator;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
 
 import br.com.caelum.vraptor.util.test.MockValidator;
 import br.com.caelum.vraptor.validator.Validator;
 
+@RunWith(MockitoJUnitRunner.class)
 public class SignupValidatorTest extends TestCase {
 
-    private UserDAO users;
-    private Validator validator;
+    private static final String VALID_EMAIL = "valid@gmail.com";
+	private static final String VALID_USER_NAME = "valid user valid user";
+	private static final String VALID_PASSWORD = "123456";
+	private Validator validator;
     private SignupValidator signupValidator;
 	private UserValidator userValidator;
 	private EmailValidator emailValidator;
 	private MessageFactory messageFactory;
-	private ResourceBundle bundle;
+	@Mock private UserDAO users;
+	@Mock private ResourceBundle bundle;
     
     @Before
     public void setup() {
-    	bundle = mock(ResourceBundle.class);
-        users = mock(UserDAO.class);
         validator = new MockValidator();
         messageFactory = new MessageFactory(bundle);
         emailValidator = new EmailValidator(validator, users, messageFactory);
         javax.validation.Validator javaxValidator = Validation.buildDefaultValidatorFactory().getValidator();
         BrutalValidator brutalValidator = new BrutalValidator(javaxValidator, validator, messageFactory);
         userValidator = new UserValidator(validator, emailValidator, messageFactory, brutalValidator);
-        signupValidator = new SignupValidator(validator, userValidator, messageFactory);
+        signupValidator = new SignupValidator(validator, userValidator, messageFactory, users);
+        when(users.existsWithEmail(VALID_EMAIL)).thenReturn(false);
+        when(users.existsWithName(VALID_USER_NAME)).thenReturn(false);
     }
 
     @Test
     public void should_verify_email() {
         when(users.existsWithEmail("used@gmail.com")).thenReturn(true);
-        User user = user("nome muito grande ai meu deus", "used@gmail.com");
-        boolean valid = signupValidator.validate(user, "123456", "123456");
+        User user = user(VALID_USER_NAME, "used@gmail.com");
+        boolean valid = signupValidator.validate(user, VALID_PASSWORD, VALID_PASSWORD);
         
         assertFalse(valid);
     }
     
     @Test
     public void should_verify_email_without_domain() {
-    	User user = user("nome muito grande ai meu deus", "usedgmail.com");
-    	boolean valid = signupValidator.validate(user, "123456", "123456");
+    	User user = user("nome muito grande ai meu deus", "invalidgmail.com");
+    	boolean valid = signupValidator.validate(user, VALID_PASSWORD, VALID_PASSWORD);
     	assertFalse(valid);
     }
     
     @Test
     public void should_verify_passwords() throws Exception {
-        when(users.existsWithEmail("valid@gmail.com")).thenReturn(false);
-        User user = user("nome muito grande ai meu deus", "valid@gmail.com");
+        when(users.existsWithEmail(VALID_EMAIL)).thenReturn(false);
+        User user = user(VALID_USER_NAME, VALID_EMAIL);
         boolean valid = signupValidator.validate(user, "123456", "1234567");
         
         assertFalse(valid);
@@ -73,18 +76,18 @@ public class SignupValidatorTest extends TestCase {
     
     @Test
     public void should_verify_tiny_password() throws Exception {
-    	when(users.existsWithEmail("valid@gmail.com")).thenReturn(false);
-    	User user = user("nome muito grande ai meu deus", "valid@gmail.com");
-    	boolean valid = signupValidator.validate(user, "123", "123");
+    	when(users.existsWithEmail(VALID_EMAIL)).thenReturn(false);
+    	User user = user(VALID_USER_NAME, VALID_EMAIL);
+    	boolean valid = signupValidator.validate(user, "12", "12");
     	
     	assertFalse(valid);
     }
     
     @Test
     public void should_verify_large_password() throws Exception {
-    	when(users.existsWithEmail("valid@gmail.com")).thenReturn(false);
+    	when(users.existsWithEmail(VALID_EMAIL)).thenReturn(false);
     	String password = 666*100 + "";
-    	User user = user("nome", "valid@gmail.com");
+    	User user = user(VALID_USER_NAME, VALID_EMAIL);
     	boolean valid = signupValidator.validate(user, password, password);
     	
     	assertFalse(valid);
@@ -92,43 +95,53 @@ public class SignupValidatorTest extends TestCase {
     
     @Test
     public void should_verify_empty_name() throws Exception {
-    	when(users.existsWithEmail("valid@gmail.com")).thenReturn(false);
-    	User user = user("", "valid@gmail.com");
-    	boolean valid = signupValidator.validate(user, "123456", "123456");
+    	when(users.existsWithEmail(VALID_EMAIL)).thenReturn(false);
+    	User user = user("", VALID_EMAIL);
+    	boolean valid = signupValidator.validate(user, VALID_PASSWORD, VALID_PASSWORD);
     	
     	assertFalse(valid);
     }
     
     @Test
     public void should_verify_large_name() throws Exception {
-    	when(users.existsWithEmail("valid@gmail.com")).thenReturn(false);
+    	when(users.existsWithEmail(VALID_EMAIL)).thenReturn(false);
     	String name = "aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa";
-    	User user = user(name, "valid@gmail.com");
-    	boolean valid = signupValidator.validate(user, "123456", "123456");
+    	User user = user(name, VALID_EMAIL);
+    	boolean valid = signupValidator.validate(user, VALID_PASSWORD, VALID_PASSWORD);
     	
     	assertFalse(valid);
     }
     
     @Test
     public void should_verify_null() throws Exception {
-        boolean valid = signupValidator.validate(null, "123", "1234");
+        boolean valid = signupValidator.validate(null, VALID_PASSWORD, VALID_PASSWORD);
         
         assertFalse(valid);
     }
     
     @Test
     public void should_verify_null_password() throws Exception {
-    	User user = user("valid user valid user", "valid@gmail.com");
+    	User user = user(VALID_USER_NAME, VALID_EMAIL);
     	boolean valid = signupValidator.validate(user, null, null);
     	
     	assertFalse(valid);
     }
     
     @Test
+    public void should_verify_if_exists_user_with_same_name() throws Exception {
+    	String name = "existentName";
+    	when(users.existsWithName(name)).thenReturn(true);
+		
+    	User user = user(name, VALID_EMAIL);
+		boolean valid = signupValidator.validate(user, VALID_PASSWORD, VALID_PASSWORD);
+    	
+    	assertFalse(valid);
+    }
+    
+    @Test
     public void should_valid_user() throws Exception {
-        when(users.existsWithEmail("used@gmail.com")).thenReturn(false);
-        User user = user("nome muito grande ai meu deus", "used@gmail.com");
-        boolean valid = signupValidator.validate(user, "123456", "123456");
+        User user = user(VALID_USER_NAME, VALID_EMAIL);
+        boolean valid = signupValidator.validate(user, VALID_PASSWORD, VALID_PASSWORD);
         
         assertTrue(valid);
     }
