61d47a5c7b649fd7f39fd213b35d9f78bcfc2025
Francisco Sokol <chico.sokol@gmail.com>
2014-08-26 18:18:28 -0300

    executing sed with args compatible with osx

--- scripts/get-version.sh
+++ scripts/get-version.sh
@@ -4,10 +4,4 @@ SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 POM=$SCRIPT_DIR/../pom.xml
 
-## Detect if advanced regex is available
-`echo 'asd   fsd' | sed -r 's/\s+(.*)/\1/' > /dev/null 2>&1`
-if [ "$?" == "1" ]; then
-  grep "version" $POM | head -n 1 | sed 's/[[:space:]]//g' | sed 's/<version>//' | sed 's/<\/version>//'
-else
-  grep "version" $POM | head -n 1 | sed -r 's/\s+<version>(.*)<\/version>/\1/'
-fi
+grep "version" $POM | head -n 1 | sed 's/[[:space:]]//g' | sed 's/<version>//' | sed 's/<\/version>//'
