79768c8587a776719a4e628cb4e636f6b768ecbe
leonardowolter <leocwolter@gmail.com>
2014-09-09 15:11:30 -0300

    fixing htmlattribute search for key

--- src/main/java/org/mamute/sanitizer/HtmlAttribute.java
+++ src/main/java/org/mamute/sanitizer/HtmlAttribute.java
@@ -11,14 +11,15 @@ import br.com.caelum.vraptor.environment.Environment;
 
 public class HtmlAttribute {
 
-
 	public static Map<String, String> using(String element, Environment environment){
 		Map<String, String> attributesAndWhitelist = new HashMap<>();
-		String allowedString = environment.get(ALLOWED_ATTRIBUTES_KEY_PREFIX+element);
-		if(allowedString != null){
+		String allowedAttributesKey = ALLOWED_ATTRIBUTES_KEY_PREFIX+element;
+		if(environment.has(allowedAttributesKey)){
+			String allowedString = environment.get(allowedAttributesKey);
 			String[] allowedAttributes = allowedString.split(HtmlElement.COMMA);
 			for (String attribute : allowedAttributes) {
-				String whitelist = environment.get(ALLOWED_ATTRIBUTES_KEY_PREFIX+element+ALLOWED_ATTRIBUTES_WHITELIST_KEY_SUFIX+attribute);
+				String whitelistKey = allowedAttributesKey+ALLOWED_ATTRIBUTES_WHITELIST_KEY_SUFIX+attribute;
+				String whitelist = environment.has(whitelistKey) ? environment.get(whitelistKey) : null;
 				attributesAndWhitelist.put(attribute, whitelist);
 			}
 		}
