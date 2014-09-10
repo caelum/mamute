91cbfc8a69198b353fed5174e59e6c8ebd685ab5
leonardowolter <leocwolter@gmail.com>
2014-09-10 10:45:52 -0300

    probably no need to hashCode and equals

--- src/main/java/org/mamute/model/MarkedText.java
+++ src/main/java/org/mamute/model/MarkedText.java
@@ -27,31 +27,6 @@ public class MarkedText {
 	}
 
 	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((marked == null) ? 0 : marked.hashCode());
-		return result;
-	}
-
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		MarkedText other = (MarkedText) obj;
-		if (marked == null) {
-			if (other.marked != null)
-				return false;
-		} else if (!marked.equals(other.marked))
-			return false;
-		return true;
-	}
-
-	@Override
 	public String toString() {
 		return "MarkedText [pure=" + pure + ", marked=" + marked + "]";
 	}
