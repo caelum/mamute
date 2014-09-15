619c84dd06ca5f932b1e9c55b004bada2e6c28ec
Francisco Sokol <chico.sokol@gmail.com>
2014-09-15 16:21:50 -0300

    excluding .git from build

--- scripts/mvn-package.sh
+++ scripts/mvn-package.sh
@@ -3,7 +3,7 @@ TEMP_FOLDER_NAME=".mamute-build"
 TEMP_FOLDER="${APPLICATION_DIR}/${TEMP_FOLDER_NAME}"
 cd ${APPLICATION_DIR}
 mvn clean
-rsync -av --delete --exclude=${TEMP_FOLDER_NAME} . ${TEMP_FOLDER} 
+rsync -av --delete --exclude=${TEMP_FOLDER_NAME} --exclude=.git . ${TEMP_FOLDER} 
 cd ${TEMP_FOLDER}
 mvn package -DskipTests
 mv target ../target
