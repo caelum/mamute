270b34b3fd0e4ec8337f19acb92b10a20de664d3
guilherme <guilherme.silveira@caelum.com.br>
2014-09-23 11:32:27 -0300

    using the default processbuilder

--- src/main/java/br/com/caelum/vraptor/server/GruntRunner.java
+++ src/main/java/br/com/caelum/vraptor/server/GruntRunner.java
@@ -1,7 +1,6 @@
 package br.com.caelum.vraptor.server;
 
-import java.io.*;
-import java.util.Scanner;
+import java.io.IOException;
 
 public class GruntRunner implements Runnable{
 
@@ -15,11 +14,8 @@ public class GruntRunner implements Runnable{
 	public void run() {
 		System.out.println("Executing grunt...");
 		try {
-			Process exec = Runtime.getRuntime().exec("mvn grunt:grunt -Dmamute.grunt.task=run");
-			new Thread(new ProcessOutputWriter(exec.getInputStream(), System.out)).run();
-			new Thread(new ProcessOutputWriter(exec.getErrorStream(), System.err)).run();
-			exec.waitFor();
-			int value = exec.exitValue();
+			Process exec = new ProcessBuilder("mvn", "grunt:grunt", "-Dmamute.grunt.task=run").inheritIO().start();
+			int value = exec.waitFor();
 			if (value != 0) {
 				System.err.println("Grunt call failed with exit value: " + value);
 			}
@@ -28,27 +24,6 @@ public class GruntRunner implements Runnable{
 			throw new RuntimeException("Couldn't run grunt", e);
 		}
 
-	}
-
-	private static class ProcessOutputWriter implements Runnable {
-		private final InputStream stream;
-		private final PrintStream out;
-
-		public ProcessOutputWriter(InputStream stream, PrintStream out) {
-			this.stream = stream;
-			this.out = out;
-		}
-
-		@Override
-		public void run() {
-			try (Scanner s = new Scanner(stream)) {
-				while (s.hasNextLine()) {
-					out.println(s.nextLine());
-				}
-			}
-
-
-		}
 	}
 
 }
