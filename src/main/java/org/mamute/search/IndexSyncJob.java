55a81b2ff2b03cd1be97209bf24e2bafb6e16900
leonardowolter <leocwolter@gmail.com>
2014-10-07 15:12:48 -0300

    indexing should not block

--- src/main/java/org/mamute/search/IndexSyncJob.java
+++ src/main/java/org/mamute/search/IndexSyncJob.java
@@ -1,56 +1,76 @@
 package org.mamute.search;
 
-import br.com.caelum.brutauth.auth.annotations.CustomBrutauthRules;
-import br.com.caelum.vraptor.Controller;
-import br.com.caelum.vraptor.Post;
-import br.com.caelum.vraptor.environment.Environment;
-import br.com.caelum.vraptor.events.VRaptorInitialized;
-
-import org.hibernate.Session;
-import org.mamute.brutauth.auth.rules.ModeratorOnlyRule;
-import org.mamute.dao.InvisibleForUsersRule;
-import org.mamute.dao.QuestionDAO;
-import org.mamute.model.LoggedUser;
-import org.mamute.model.Question;
-import org.mamute.model.User;
-import org.mamute.providers.SessionFactoryCreator;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
+import java.io.IOException;
+import java.util.List;
 
 import javax.enterprise.context.ApplicationScoped;
 import javax.enterprise.event.Observes;
 import javax.inject.Inject;
 
-import java.util.List;
+import org.apache.commons.httpclient.HttpClient;
+import org.apache.commons.httpclient.methods.PostMethod;
+import org.mamute.brutauth.auth.rules.ModeratorOnlyRule;
+import org.mamute.dao.QuestionDAO;
+import org.mamute.model.Question;
+import org.mamute.vraptor.Linker;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
-import static org.mamute.model.SanitizedText.fromTrustedText;
+import br.com.caelum.brutauth.auth.annotations.CustomBrutauthRules;
+import br.com.caelum.vraptor.Controller;
+import br.com.caelum.vraptor.Post;
+import br.com.caelum.vraptor.Result;
+import br.com.caelum.vraptor.environment.Environment;
+import br.com.caelum.vraptor.events.VRaptorInitialized;
 
 @Controller @ApplicationScoped
 @CustomBrutauthRules(ModeratorOnlyRule.class)
 public class IndexSyncJob {
 	public static final Logger LOGGER = LoggerFactory.getLogger(IndexSyncJob.class);
 
-	private SessionFactoryCreator factory;
 	private QuestionIndex index;
 	private Environment environment;
+	private Linker linker;
+	private Result result;
+	private QuestionDAO questions;
+
 
 	@Deprecated
 	protected IndexSyncJob() {
 	}
 
 	@Inject
-	public IndexSyncJob(SessionFactoryCreator factory, QuestionIndex index, Environment environment) {
-		this.factory = factory;
+	public IndexSyncJob(QuestionIndex index, Environment environment,
+			Linker linker, Result result, QuestionDAO questions) {
 		this.index = index;
 		this.environment = environment;
+		this.linker = linker;
+		this.result = result;
+		this.questions = questions;
 	}
-
+	
+	@Post("/msdf0fhq924pqpsdl")
+	public void indexSync() {
+		linker.linkTo(this).execute();
+		final PostMethod postMethod = new PostMethod(linker.get());
+		LOGGER.info("Running thread to sync solr indexes");
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				LOGGER.info("Solr indexes thread started!");
+				HttpClient httpClient = new HttpClient();
+				try {
+					httpClient.executeMethod(postMethod);
+				} catch (IOException e) {
+					LOGGER.debug("Couldn't execute post "+postMethod);
+				}
+			}
+		}).start();
+		result.nothing();
+	}
+		
 	@Post("/akwiqeojovndfasf0asf0s9ad8fas9d")
 	public void execute() {
-		Session session = factory.getInstance().openSession();
-		try {
-			QuestionDAO questions = new QuestionDAO(session, generateUser());
-
 			long pages = questions.numberOfPages();
 			long total = 0;
 			LOGGER.info("Syncing questions!");
@@ -60,21 +80,13 @@ public class IndexSyncJob {
 				total += q.size();
 			}
 			LOGGER.info("Synced " + total + " questions");
-		} finally {
-			session.close();
-		}
+			result.nothing();
 	}
 
 	@Post("/akwiqeojovndfasf0asf0s9ad8fas9d12io3nwo120")
 	public void onStartup(@Observes VRaptorInitialized init) {
 		if (environment.supports("solr.syncOnStartup") && environment.supports("feature.solr")) {
-			execute();
+			indexSync();
 		}
-	}
-
-	private InvisibleForUsersRule generateUser() {
-		User user = new User(fromTrustedText("System"), "system");
-		LoggedUser loggedUser = new LoggedUser(user, null);
-		return new InvisibleForUsersRule(loggedUser);
 	}
 }
