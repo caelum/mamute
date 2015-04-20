16c3d6002d9fd48ec8f46f49d6cf3292183703f9
Chico Sokol <chico.sokol@gmail.com>
2015-04-19 22:26:11 -0300

    Fixing validation error msg

--- src/main/webapp/assets/js/delete-question.js
+++ src/main/webapp/assets/js/delete-question.js
@@ -6,10 +6,9 @@ $(function() {
             url: "/question/" + id,
             method: "DELETE",
             complete: function(xhr, status) {
-                debugger;
                 if (xhr.status == 200) {
                     document.location.href = "/?message=question.delete.confirmation";
-                } else if (xhr == 400) {
+                } else if (xhr.status == 400) {
                     alert(xhr.responseText);
                 } else {
                     alert("An unknown error occurred");
