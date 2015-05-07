5db24847fc65c8a5389d4b155059599a33db5fea
Chico Sokol <chico.sokol@gmail.com>
2015-05-06 23:19:09 -0300

    Removing fb username and raising exception in case facebook response is != 2xx

--- src/main/java/org/mamute/auth/FacebookAPI.java
+++ src/main/java/org/mamute/auth/FacebookAPI.java
@@ -12,17 +12,17 @@ import com.google.gson.JsonObject;
 import com.google.gson.JsonParser;
 
 public class FacebookAPI implements SocialAPI{
-	
+
 	private final Token accessToken;
 	private final OAuthService service;
-	
+
 	public FacebookAPI(OAuthService service, Token accessToken) {
 		this.service = service;
 		this.accessToken = accessToken;
 	}
 
 	public Optional<SignupInfo> getSignupInfo() {
-		String url = "https://graph.facebook.com/me?fields=name,email,location,username,id";
+		String url = "https://graph.facebook.com/me?fields=name,email,location,id";
 		Response response = makeRequest(url);
 		JsonObject jsonObject = new JsonParser().parse(response.getBody()).getAsJsonObject();
 		return SignupInfo.fromFacebook(jsonObject);
@@ -32,9 +32,6 @@ public class FacebookAPI implements SocialAPI{
 		String url = "https://graph.facebook.com/me?fields=id";
 		Response response = makeRequest(url);
 		String body = response.getBody();
-		if (response.getCode() != 200) {
-			throw new IllegalArgumentException("http error: " + response.getCode() + ", facebook response body: " + body);
-		}
 		JsonObject jsonObj = new JsonParser().parse(body).getAsJsonObject();
 		JsonElement jsonElement = jsonObj.get("id");
 		if (jsonElement == null) {
@@ -47,6 +44,10 @@ public class FacebookAPI implements SocialAPI{
 		OAuthRequest request = new OAuthRequest(Verb.GET, url);
 		service.signRequest(accessToken, request);
 		Response response = request.send();
+		String body = response.getBody();
+		if (response.getCode() / 100 != 2) {
+			throw new IllegalArgumentException("http error: " + response.getCode() + ", facebook response body: " + body);
+		}
 		return response;
 	}
 
