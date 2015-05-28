35d9e3728d008389cdbb184e8f30949150bb1ec1
Chico Sokol <chico.sokol@gmail.com>
2015-05-27 23:15:49 -0300

    Improving logging

--- src/main/java/org/mamute/util/UserLogger.java
+++ src/main/java/org/mamute/util/UserLogger.java
@@ -1,7 +1,9 @@
 package org.mamute.util;
 
 import br.com.caelum.vraptor.events.MethodReady;
+import com.google.common.base.Objects;
 import org.apache.log4j.Logger;
+import org.mamute.infra.ClientIp;
 import org.mamute.model.LoggedUser;
 import org.mamute.model.User;
 
@@ -17,6 +19,8 @@ public class UserLogger {
 	private LoggedUser loggedUser;
 	@Inject
 	private HttpServletRequest request;
+	@Inject
+	private ClientIp clientIp;
 
 	private static Logger LOG = Logger.getLogger(UserLogger.class);
 
@@ -29,11 +33,16 @@ public class UserLogger {
 			userName = format("%s (id=%s)", name, id);
 		}
 
-		String realIp = request.getHeader("X-Real-IP");
+		String realIp = clientIp.get();
 		String uri = request.getRequestURI();
-		String method = request.getMethod();
+		String method = getMethod();
 		String log = format("%s -> %s from %s %s", method, uri, userName,  realIp);
 		LOG.info(log);
 
+	}
+
+	private String getMethod() {
+		String forcedMethod = request.getParameter("_method");
+		return Objects.firstNonNull(forcedMethod, request.getMethod());
 	}
 }
