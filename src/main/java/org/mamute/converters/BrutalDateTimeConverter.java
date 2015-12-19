199800406c2943c3314ced80186f2807de8f744d
Martin Blumenstingl <martin.blumenstingl@googlemail.com>
2015-12-19 23:32:40 +0100

    Don't use a regex to validate the date pattern BrutalDateTimeConverter
    This makes sure that we don't have to adjust the regex whenever the
    date format is changed.

--- src/main/java/org/mamute/converters/BrutalDateTimeConverter.java
+++ src/main/java/org/mamute/converters/BrutalDateTimeConverter.java
@@ -1,5 +1,9 @@
 package org.mamute.converters;
 
+import static com.google.common.base.Strings.isNullOrEmpty;
+
+import java.util.Locale;
+
 import javax.enterprise.inject.Specializes;
 import javax.inject.Inject;
 
@@ -14,37 +18,47 @@ import br.com.caelum.vraptor.converter.jodatime.DateTimeConverter;
 import br.com.caelum.vraptor.simplemail.template.BundleFormatter;
 import br.com.caelum.vraptor.validator.Validator;
 
-@Convert(DateTime.class) @Specializes
+@Convert(DateTime.class)
+@Specializes
 public class BrutalDateTimeConverter extends DateTimeConverter{
 
-	@Inject private Validator validator;
-	@Inject private MessageFactory messageFactory;
-	@Inject private BundleFormatter bundle;
+	private Validator validator;
+	private MessageFactory messageFactory;
+	private BundleFormatter bundle;
+
+	@Deprecated
+	public BrutalDateTimeConverter() {
+		super();
+	}
+
+	@Inject
+	public BrutalDateTimeConverter(Locale locale, Validator validator, MessageFactory messageFactory, BundleFormatter bundle)
+	{
+		super(locale);
+		this.validator = validator;
+		this.messageFactory = messageFactory;
+		this.bundle = bundle;
+	}
 
 	@Override
 	public DateTime convert(String value, Class<? extends DateTime> type) {
-		
-		if (value == null || value.isEmpty()){
+		if (isNullOrEmpty(value)) {
 			return null;
 		}
-		String[] splitedDate = value.split("/");
-		if(splitedDate.length > 3 
-				|| value.length() > 10 
-				|| !value.matches("^\\d\\d/\\d\\d/\\d\\d\\d\\d$")) {
-			validator.add(messageFactory.build("error", "converters.errors.invalid_date.format"));
-		}
-		
-		if (validator.hasErrors()) {
-			return null;
-		}
-		
-		DateTimeFormatter pattern = DateTimeFormat.forPattern(bundle.getMessage("date.joda.simple.pattern"));
+
 		try {
-			DateTime date = pattern.parseDateTime(value);
-			return date;
-		}catch (IllegalFieldValueException e) {
+			return getFormatter().parseDateTime(value);
+		} catch (IllegalFieldValueException e) {
 			validator.add(messageFactory.build("error", "converters.errors.invalid_date.parameters"));
 		}
+		catch (IllegalArgumentException e) {
+			validator.add(messageFactory.build("error", "converters.errors.invalid_date.format"));
+		}
 		return null;
+	}
+
+	@Override
+	protected DateTimeFormatter getFormatter() {
+		return DateTimeFormat.forPattern(bundle.getMessage("date.joda.simple.pattern"));
 	}
 }
