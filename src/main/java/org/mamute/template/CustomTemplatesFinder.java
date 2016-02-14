028885ebc76c72f0808bd276090d7c0fd7ce0d79
Felipe Oliveira <felipeweb.programador@gmail.com>
2016-02-14 20:52:06 -0200

    Refactoring custom template finder

--- src/main/java/org/mamute/template/CustomTemplatesFinder.java
+++ src/main/java/org/mamute/template/CustomTemplatesFinder.java
@@ -13,16 +13,12 @@ import javax.servlet.ServletContext;
 public class CustomTemplatesFinder {
 	protected static final String CUSTOM_TEMPLATE_URI = "/WEB-INF/jsp/theme/custom/";
 	protected static final String EXTENSION = ".jspf";
-	private File customTemplate;
 	@Inject
 	private ServletContext application;
 
 	public boolean exists(String fileName) {
-		customTemplate = new File(application.getRealPath(CUSTOM_TEMPLATE_URI
+		File customTemplate = new File(application.getRealPath(CUSTOM_TEMPLATE_URI
 				+ fileName + EXTENSION));
-		if (customTemplate.exists()) {
-			return true;
-		}
-		return false;
+		return customTemplate.exists();
 	}
 }
